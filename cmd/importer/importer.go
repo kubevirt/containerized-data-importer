21ab9b42b01373035d815da6b1391657301d6c46
Jon Cope <jcope@redhat.com>
2018-01-24 14:04:40 -0600

    Makefile builds correctly, added data struct, do decode

--- cmd/importer/importer.go
+++ cmd/importer/importer.go
@@ -3,40 +3,56 @@ package main
 import (
 	"flag"
 	"github.com/golang/glog"
+	"io"
 	"os"
 	"strings"
-	"io"
 )
 
 // importer.go implements a data fetching service capable of pulling objects from remote object stores
 // and writing to a local directory.  It utilizes the minio-go client sdk.
 // This process expects several environmental variables:
-//    IMPORTER_ENDPOINT       URL of object server
+//    IMPORTER_URL            Full url + path to object. Mutually exclusive with IMPORTER_ENDPOINT
 //    IMPORTER_OBJECT_PATH    Full path of object (e.g. bucket/object)
 //    IMPORTER_ACCESS_KEY_ID  Secret key is the password to your account
 //    IMPORTER_SECRET_KEY     Access key is the user ID that uniquely identifies your account.
 
 const (
+	IMPORTER_URL           = "IMPORTER_URL"
 	IMPORTER_ENDPOINT      = "IMPORTER_ENDPOINT"
 	IMPORTER_OBJECT_PATH   = "IMPORTER_OBJECT_PATH"
 	IMPORTER_ACCESS_KEY_ID = "IMPORTER_ACCESS_KEY_ID"
 	IMPORTER_SECRET_KEY    = "IMPORTER_SECRET_KEY"
 )
 
+type importInfo struct {
+	url         string
+	endpoint    string
+	objectPath  string
+	accessKeyId string
+	secretKey   string
+}
+
 func init() {
 	// TODO verify destination volume exists (and is mountpoint?)
 	flag.Parse()
 }
-
+// asfdasdfsadfasdf
 func main() {
 	defer glog.Flush()
-	reader := getDataWithClient(
-		parseEnvVar(IMPORTER_ENDPOINT),
-		parseEnvVar(IMPORTER_OBJECT_PATH),
-		parseEnvVar(IMPORTER_ACCESS_KEY_ID),
-		parseEnvVar(IMPORTER_SECRET_KEY))
+	imp := &importInfo{
+		endpoint:    parseEnvVar(IMPORTER_ENDPOINT, false),
+		objectPath:  parseEnvVar(IMPORTER_OBJECT_PATH, false),
+		accessKeyId: parseEnvVar(IMPORTER_ACCESS_KEY_ID, false),
+		secretKey: parseEnvVar(IMPORTER_SECRET_KEY, false),
+	}
+	if len(imp.endpoint) > 0 && len(imp.url) > 0 {
+		glog.Fatalf("Detected IMPORTER_URL and IMPORTER_ENDPOINT non-nil values. Variables are mutually exclusive.")
+	} else if len(imp.endpoint) > 0 {
+		// TODO check required vars
+	}
+	reader := getDataWithClient(imp)
 	defer reader.Close()
-	obj := parseEnvVar(IMPORTER_OBJECT_PATH)
+	obj := parseEnvVar(IMPORTER_OBJECT_PATH, false)
 	objSlice := strings.Split(obj, "/")
 	obj = objSlice[len(objSlice)-1]
 	outFile, err := os.Create(obj)
