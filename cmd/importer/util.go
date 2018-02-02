fd1ef75c6e242f1dc19a7dc9c80f5583d33fdf4c
Jon Cope <jcope@redhat.com>
2018-02-02 09:52:54 -0600

    Refactored filewrite code, added url support

--- cmd/importer/util.go
+++ cmd/importer/util.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"github.com/golang/glog"
 	"github.com/minio/minio-go"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
+	"path/filepath"
 	"strings"
-	"encoding/base64"
 )
 
 func parseEnvVar(envVarName string, decode bool) string {
@@ -23,26 +25,55 @@ func parseEnvVar(envVarName string, decode bool) string {
 	return value
 }
 
-func getDataWithClient(con *importInfo) io.ReadCloser {
-	mc, err := minio.NewV4(con.endpoint, con.accessKeyId, con.secretKey, false)
+func getDataWithClient(importInfo *importInfo) io.ReadCloser {
+	mc, err := minio.NewV4(importInfo.endpoint, importInfo.accessKeyId, importInfo.secretKey, false)
 	if err != nil {
 		glog.Fatalf("func getDataWithClient: Could not create Minio client: %v", err)
 	}
-	objPath := strings.Split(con.objectPath, "/")
-	bucketName := objPath[0]
-	objName := strings.Join(objPath[1:], "_")
-	glog.Infof("Streaming object %s", con.objectPath)
-	objectReader, err := mc.GetObject(bucketName, objName, minio.GetObjectOptions{})
+	bucket, object, err := parseDataPath(importInfo.objectPath, false)
+	glog.Infof("Streaming object %s", importInfo.objectPath)
+	objectReader, err := mc.GetObject(bucket, object, minio.GetObjectOptions{})
 	if err != nil {
 		glog.Fatalf("func getDataWithClient: failed getting objectPath: %v", err)
 	}
 	return objectReader
 }
 
-func getDataWithHTTP(url string) io.ReadCloser {
-	resp, err := http.Get(url)
+func getDataWithHTTP(importInfo *importInfo) io.ReadCloser {
+	resp, err := http.Get(importInfo.url)
 	if err != nil {
 		glog.Fatalf("func streamDataFromURL: response body error: %v", err)
 	}
 	return resp.Body
+}
+
+func parseDataPath(dataPath string, fromUrl bool) (string, string, error) {
+	var bucket, filename string
+	if fromUrl {
+		url, err := url.Parse(dataPath)
+		if err != nil {
+			return "", "", fmt.Errorf("func parseDataPath: %s", err)
+		}
+		pathSlice := strings.Split(url.Path, "/")
+		bucket = ""
+		filename = pathSlice[len(pathSlice)-1]
+		return bucket, filename, nil
+	}
+	pathSlice := strings.Split(dataPath, "/")
+	bucket = pathSlice[0]
+	// Rejoin object name. Convert / to _
+	filename = strings.Join(pathSlice[1:], "_")
+	return bucket, filename, nil
+}
+
+func streamDataToFile(dataReader io.ReadCloser, filename string) error {
+	outFile, err := os.Create(filepath.Join(WRITE_PATH, filename))
+	defer outFile.Close()
+	if err != nil {
+		return fmt.Errorf("func streamDataToFile: create file error: %v", err)
+	}
+	if _, err = io.Copy(outFile, dataReader); err != nil {
+		return fmt.Errorf("func streamDataToFile: error streaming data: %v", err)
+	}
+	return nil
 }
