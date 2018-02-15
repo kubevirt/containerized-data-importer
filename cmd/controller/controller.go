001b1c64e04b72cd4bda09e14df9b8df9c36895a
Jon Cope <jcope@redhat.com>
2018-02-15 17:32:40 -0600

    Controller detects pvcs and calls empty work func (processItem).

--- cmd/controller/controller.go
+++ cmd/controller/controller.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"flag"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"time"
+
 	"github.com/golang/glog"
 	"github.com/kubevirt/containerized-data-importer/pkg/controller"
 	"github.com/kubevirt/containerized-data-importer/pkg/util"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
-	"os"
-	"path/filepath"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
 )
 
 var (
@@ -31,9 +38,52 @@ func main() {
 		glog.Infoln("Detected in-cluster environment")
 		client = util.GetInClusterClient()
 	} else {
-		glog.Infoln("Deteced out-of-cluster environment")
+		glog.Infoln("Detected out-of-cluster environment")
 		client = util.GetOutOfClusterClient(configPath, masterURL)
 	}
-	_ = controller.NewController(client)
 
+	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	informerFactory := informers.NewSharedInformerFactory(client, time.Second*30)
+	pvcInformer := informerFactory.Core().V1().PersistentVolumeClaims().Informer()
+	pvcInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			if err == nil {
+				queue.AddRateLimited(key)
+			}
+		},
+		UpdateFunc: func(old, new interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(new)
+			if err == nil {
+				queue.AddRateLimited(key)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			if err == nil {
+				queue.AddRateLimited(key)
+			}
+		},
+	})
+
+	pvcListWatcher := cache.NewListWatchFromClient(client.CoreV1().RESTClient(), "persistentvolumeclaims", "", fields.Everything())
+	cdiController := controller.NewController(client, queue, pvcInformer, pvcListWatcher)
+	stopCh := handleSignals()
+	err := cdiController.Run(1, stopCh)
+	if err != nil {
+		glog.Fatalln(err)
+	}
+}
+
+func handleSignals() <-chan struct{} {
+	sigCh := make(chan os.Signal)
+	stopCh := make(chan struct{})
+	go func() {
+		signal.Notify(sigCh)
+		<-sigCh
+		close(stopCh)
+		os.Exit(1)
+	}()
+	return stopCh
 }
--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,12 +1,14 @@
 package controller
 
 import (
-	"k8s.io/apimachinery/pkg/fields"
-	"k8s.io/client-go/informers"
+	"fmt"
+	"time"
+
+	"github.com/golang/glog"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
-	"time"
 )
 
 type Controller struct {
@@ -16,11 +18,12 @@ type Controller struct {
 	pvcListWatcher cache.ListerWatcher
 }
 
-func NewController(client kubernetes.Interface) *Controller {
-	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
-	informerFactory := informers.NewSharedInformerFactory(client, time.Second*30)
-	pvcInformer := informerFactory.Core().V1().PersistentVolumeClaims().Informer()
-	pvcListWatcher := cache.NewListWatchFromClient(client.CoreV1().RESTClient(), "persistentvolumeclaims", "", fields.Everything())
+func NewController(
+	client kubernetes.Interface,
+	queue workqueue.RateLimitingInterface,
+	pvcInformer cache.SharedIndexInformer,
+	pvcListWatcher cache.ListerWatcher,
+) *Controller {
 	return &Controller{
 		clientset:      client,
 		queue:          queue,
@@ -29,17 +32,61 @@ func NewController(client kubernetes.Interface) *Controller {
 	}
 }
 
-func (c *Controller) Start(configPath string) {}
+func (c *Controller) Run(threadiness int, stopCh <-chan struct{}) error {
+	defer c.queue.ShutDown()
+	glog.Infoln("Starting CDI controller loop")
+	if threadiness < 1 {
+		return fmt.Errorf("controller.Run(): expected >0 threads, got %d", threadiness)
+	}
+	go c.pvcInformer.Run(stopCh)
+	if !cache.WaitForCacheSync(stopCh, c.pvcInformer.HasSynced) {
+		return fmt.Errorf("controller.Run(): Timeout waiting for cache sync")
+	}
+	glog.Infoln("Controller cache has synced")
 
-func (c *Controller) Run(stopCh <-chan struct{}) {}
-
-func (c *Controller) runWorker() {}
-
-func (c *Controller) processNextItem() bool { return true }
-
-func (c *Controller) processItem(key, kobj string) error {
-	c.pvcInformer.GetIndexer().GetByKey(key)
+	for i := 0; i < threadiness; i++ {
+		go wait.Until(c.runWorkers, time.Second, stopCh)
+	}
+	<-stopCh
 	return nil
 }
 
-func (c *Controller) HasSynced() bool { return true }
+func (c *Controller) runWorkers() {
+	glog.Infoln("DEBUG - controller.runWorkers()")
+	for c.processNextItem() {
+	}
+}
+
+func (c *Controller) processNextItem() bool {
+	glog.Infoln("DEBUG -- controller.processNextItem()")
+	key, shutdown := c.queue.Get()
+	if shutdown {
+		return false
+	}
+	glog.Infof("Got Object: %v", key)
+	defer c.queue.Done(key)
+	if key, ok := key.(string); !ok {
+		c.queue.Forget(key)
+		glog.Errorf("controller.processNextItem(): key object failed string type assertion")
+		return false
+	}
+	if err := c.processItem(key.(string)); err != nil {
+		c.queue.Forget(key)
+		glog.Errorf("controller.processNextItem(): error processing key: %v", err)
+	}
+	return true
+}
+
+func (c *Controller) processItem(key string) error {
+	glog.Infof("DEBUG -- controller.processItem(): processing object %q", key)
+	obj, ok, err := c.pvcInformer.GetIndexer().GetByKey(key)
+	c.pvcInformer.GetController()
+	if err != nil {
+		return fmt.Errorf("controller.processItem(): error getting object with key %s: %v", key, err)
+	}
+	if !ok {
+		glog.Infof("Object with key %s does not exist\n", key)
+	}
+	c.queue.Forget(obj)
+	return nil
+}
