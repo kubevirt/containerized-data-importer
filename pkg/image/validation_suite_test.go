447177a506a3a5201ba795c0903b1aa310666cce
Jon Cope <jcope@redhat.com>
2018-03-29 19:03:57 -0500

    Add functional testing for supported compression and archiving (excl qcow2)
    Fixed 'unexpected EOF' when reading test output (use io.Copy instead of ioutil.ReadAll), Test that input and output byte slices contain the same data
    
    Extracted slice vars from loop
    
    Fixed CreateTarXz/Gz funcs with correct calls (currently unimplemented)
    
    Moved datastream tests to separate directory in prep for container work
    
    Distinguish tests with build tags (unit v functional)
    Add make recipe to functional test compilation, image build, and run
    
    Improved Makefile output
    
    Converted gz in-mem test to file read test, paremeterized importer Dockerfile to enable use as
    test env, added build-args to Makefile for binary specific options.
    
    Added xz test, condenced BeforeEach into It block (looping over BeforeEach causes unwanted behaviours)
    
    Abstracted compression to single function, removed FileExt type (back to
    strings), added tar test
    
    Support nested format, add extention to convert function map
    create base file with name provided in test table, no constant
    added qcow2 conversion (broken: decompressed file is bigger than
    original)
    
    Extracted some setup to helper, add .tar.xz test

--- pkg/image/validation_suite_test.go
+++ pkg/image/validation_suite_test.go
@@ -1,3 +1,5 @@
+// +build unit
+
 package image_test
 
 import (
---
+++ test/datastream/datastream_test.go
@@ -0,0 +1,132 @@
+package datastream
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"strings"
+
+	"github.com/kubevirt/containerized-data-importer/pkg/image"
+	f "github.com/kubevirt/containerized-data-importer/test/framework"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+type testCase struct {
+	testDesc      string
+	srcData       io.Reader
+	inFileName    string
+	expectFormats []string
+}
+
+type Tests []testCase
+
+var _ = Describe("Streaming Data Conversion", func() {
+
+	Context("when data is in a supported file format", func() {
+
+		const (
+			size           = 16000 // 16Kb buf size
+			commonBaseName = "testFile"
+		)
+
+		// Test Table
+		tests := Tests{
+			{
+				testDesc:      "should decompress gzip",
+				inFileName:    commonBaseName,
+				expectFormats: []string{image.ExtGz},
+			},
+			{
+				testDesc:      "should decompress xz",
+				inFileName:    commonBaseName,
+				expectFormats: []string{image.ExtXz},
+			},
+			{
+				testDesc:      "should unarchive tar",
+				inFileName:    commonBaseName,
+				expectFormats: []string{image.ExtTar},
+			},
+			{
+				testDesc:      "should unpack .tar.gz",
+				inFileName:    commonBaseName,
+				expectFormats: []string{image.ExtTar, image.ExtGz},
+			},
+			{
+				testDesc:      "should unpack .tar.xz",
+				inFileName:    commonBaseName,
+				expectFormats: []string{image.ExtTar, image.ExtXz},
+			},
+			{
+				testDesc:      "should pass through unformatted data",
+				inFileName:    commonBaseName,
+				expectFormats: []string{""},
+			},
+		}
+
+		for _, t := range tests {
+
+			desc := t.testDesc
+			ff := t.expectFormats
+			fn := t.inFileName
+
+			It(desc, func() {
+
+				By("Generating some input data")
+				sampleData, err := generateTestFile(size, fn)
+
+				By(fmt.Sprintf("Converting sample file to format: %v", ff))
+				// Generate the expected data format from the random bytes
+				sampleFilename, err := f.FormatTestData(fn, ff...)
+				Expect(err).NotTo(HaveOccurred(), "Error formatting test data.")
+
+				By(fmt.Sprintf("Confirming sample file name %q matches expected file name %q", sampleFilename, fn+strings.Join(ff, "")))
+				Expect(sampleFilename).To(Equal(fn+strings.Join(ff, "")), "Test data filename doesn't match expected file name.")
+
+				// BEGIN TEST
+				By("Opening sample file for test.")
+				// Finally, open the file for reading
+				sampleFile, err := os.Open(sampleFilename)
+				Expect(err).NotTo(HaveOccurred(), "Failed to open sample file %s", sampleFilename)
+				defer sampleFile.Close()
+
+				By("Passing file reader to the data stream")
+				r, err := image.UnpackData(sampleFilename, sampleFile)
+				Expect(err).NotTo(HaveOccurred())
+				defer r.Close()
+
+				var output bytes.Buffer
+				io.Copy(&output, r)
+				By("Checking the output of the data stream")
+				Expect(err).NotTo(HaveOccurred(), "ioutil.ReadAll erred")
+				Expect(output.Len()).To(Equal(size))
+				Expect(output.Bytes()).To(Equal(sampleData))
+				By("Closing sample test file.")
+			})
+		}
+	})
+})
+
+func generateTestFile(size int, filename string) ([]byte, error){
+	// Create a some random data to compress and/or archive.
+	By("Generating test data")
+	sampleData := make([]byte, size)
+	if _, err := rand.Read(sampleData); err != nil {
+		return nil,fmt.Errorf("error occurred during rand.Read()")
+	}
+
+	// Write the byte slice to a file at /
+	// Trigger the defer Close before calling FormatTestData
+	By("Writing test data to file")
+	file, err := os.Create(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create test file %q", filename)
+	}
+	defer file.Close()
+	if _, err = file.Write(sampleData); err != nil {
+		return nil, fmt.Errorf("failed to write sample data to file")
+	}
+	return sampleData, nil
+}
