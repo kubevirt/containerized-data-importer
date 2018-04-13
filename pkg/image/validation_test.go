f09aabccd0958706fecda942242c5285ca447904
jeff vance <jeff.h.vance@gmail.com>
2018-04-12 18:57:49 -0700

    consolidate magic code w/ validation

--- pkg/image/validation_test.go
+++ pkg/image/validation_test.go
@@ -1,64 +1,128 @@
 package image_test
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"math/rand"
+	"path/filepath"
 
 	. "github.com/kubevirt/containerized-data-importer/pkg/image"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Validate file extensions", func() {
+var _ = Describe("Validate file and extensions", func() {
 	type testT struct {
-		imageFileName string
+		name    string
+		expctOK bool
 	}
 
-	Context("IsValidImageFile: valid image name will succeed", func() {
+	Context("various file extensions", func() {
 		tests := []testT{
 			{
-				imageFileName: "test.qcow2",
+				name: "test.qcow2",
+				expctOK: true,
 			},
 			{
-				imageFileName: "test.tar",
+				name: "test.tar",
+				expctOK: true,
 			},
 			{
-				imageFileName: "test.gz",
+				name: "test.gz",
+				expctOK: true,
 			},
 			{
-				imageFileName: "test.xz",
+				name: "test.xz",
+				expctOK: true,
 			},
 			{
-				imageFileName: "test.img",
+				name: "test.img",
+				expctOK: true,
 			},
 			{
-				imageFileName: "test.iso",
+				name: "test.iso",
+				expctOK: true,
+			},
+			{
+				name: "xyz.abc",
+				expctOK: false,
+			},
+			{
+				name: "xxx",
+				expctOK: false,
+			},
+			{
+				name: "",
+				expctOK: false,
 			},
 		}
+
 		for _, t := range tests {
-			name := t.imageFileName
+			name := t.name
+			expctOK := t.expctOK
+			ext := filepath.Ext(name)
+
 			It(fmt.Sprintf("checking filename %q", name), func() {
-				Expect(IsSupporedFileType(name)).Should(BeTrue())
+				supported := IsSupporedFileType(name)
+				if expctOK {
+					Expect(supported).To(BeTrue(), fmt.Sprintf("%q should be a supported extension", ext))
+				} else {
+					Expect(supported).ToNot(BeTrue(), fmt.Sprintf("%q should not be a supported extension", ext))
+				}
 			})
 		}
 	})
 
-	Context("IsValidImageFile: invalid image name will fail", func() {
+	Context("QCOW2 magic number", func() {
+		type testT struct {
+			size       int
+			expctMatch bool
+		}
 		tests := []testT{
 			{
-				imageFileName: "xyz.abc",
+				size:       512,
+				expctMatch: true,
 			},
 			{
-				imageFileName: "xxx",
+				size:       QCOW2MagicStrSize,
+				expctMatch: true,
 			},
 			{
-				imageFileName: "",
+				size:       1,
+				expctMatch: false,
 			},
 		}
+
 		for _, t := range tests {
-			name := t.imageFileName
-			It(fmt.Sprintf("checking filename %q", name), func() {
-				Expect(IsSupporedFileType(name)).ShouldNot(BeTrue())
+			size := t.size
+			expctQcow2 := t.expctMatch
+			It(fmt.Sprintf("%d byte buffer", size), func() {
+				f := createQCOW2(size)
+				magic, err := GetMagicNumber(f)
+				Expect(err).ToNot(HaveOccurred(), "GetMagicNumber error:")
+				match := MatchQcow2MagicNum(magic)
+				if expctQcow2 {
+					Expect(match).To(BeTrue(), fmt.Sprintf("magic num %q did not match expected %q", magic, QCOW2MagicStr))
+				} else {
+					Expect(match).ToNot(BeTrue(), fmt.Sprintf("magic num %q should not have been found", QCOW2MagicStr))
+				}
 			})
 		}
 	})
 })
+
+// Return a reader for the magic number. Size is how big to make the buffer.
+func createQCOW2(size int) io.Reader {
+	if size <= 0 {
+		return nil
+	}
+	buf := make([]byte, size)
+	_, err := rand.Read(buf) // fill buf with random stuff
+	Expect(err).ToNot(HaveOccurred(), "createQCOW2: rand.Read errror:")
+	// set Qcow2 magic num
+	for i := 0; i < QCOW2MagicStrSize && i < size; i++ {
+		buf[i] = QCOW2MagicStr[i]
+	}
+	return bytes.NewReader(buf)
+}
