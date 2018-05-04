cac3b075be3a576f7ec5f6c9175f4120b418ce1f
Jon Cope <jcope@redhat.com>
2018-05-04 11:21:16 -0500

    Format erros with file name, line number, and function along with optional custom message

---
+++ pkg/util/errors.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+	"fmt"
+	"runtime"
+)
+
+type err struct {
+	msg        string
+	file       string
+	function   string
+	lineNumber int
+}
+
+func Err(msg string) *err {
+	pc := make([]uintptr, 1) // Only store the PC for the caller of Err().
+	runtime.Callers(2, pc)   // skip the 2 lowest PCs (Callers() and Err).
+	frames := runtime.CallersFrames(pc)
+	callerFrame, _ := frames.Next() // Get the frame of the caller of Err.
+
+	return &err{
+		msg:        msg,
+		file:       callerFrame.File,
+		function:   callerFrame.Function,
+		lineNumber: callerFrame.Line,
+	}
+}
+
+func (e err) Error() string {
+	return fmt.Sprintf("ERROR: <%s>%s()L%d: %s", e.file, e.function, e.lineNumber, e.msg)
+}
