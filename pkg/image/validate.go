c451a0f2fa8192697adc9426b58a7282f0d7d927
jeff vance <jeff.h.vance@gmail.com>
2018-06-13 15:04:02 -0700

    wip: infer file formats

--- pkg/image/validate.go
+++ pkg/image/validate.go
@@ -1,7 +1,13 @@
 package image
 
 import (
+	"bytes"
+	"encoding/hex"
+	"io"
+	"io/ioutil"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
 const (
@@ -74,4 +80,56 @@ func IsSupportedCompressArchiveType(fn string) bool {
 // Return string as lowercase with all spaces removed.
 func TrimString(s string) string {
 	return strings.ToLower(strings.TrimSpace(s))
+}
+
+// supported file format magic strings;
+var (
+	GzMagicStr    = []byte{0x1F, 0x8B}
+	Qcow2MagicStr = []byte{'Q', 'F', 'I', 0xFB}
+	TarMagicStr   = []byte{0x75, 0x73, 0x74, 0x61, 0x72, 0x00, 0x30, 0x30} // ustar.00  OR just 6 bytes
+	XzMagicStr    = []byte{0xFD, 0x37, 0x7A, 0x58, 0x5A, 0x00, 0x00}
+)
+
+type offSizeT struct{
+	off int
+	size int
+}
+type magicMapT map[string]offSizeT
+var magicMap = magicMapT {
+	ExtImg:   offSizeT{off: 0, size: 0},
+	ExtGz:    offSizeT{off: 0, size: len(GzMagicStr)},
+	ExtQcow2: offSizeT{off: 0, size: len(Qcow2MagicStr)},
+	ExtTar:   offSizeT{off: 257, size: len(TarMagicStr)},
+	ExtXz:    offSizeT{off: 0, size: len(XzMagicStr)},
+}
+
+func FileFormat(ext string, r io.ReadCloser) (string, io.ReadCloser, error) {
+	offSize, ok := magicMap[ext]
+	if !ok || offSize.size == 0 {
+		return "", r, nil
+	}
+	magic, rdr, err := GetMagicString(r, offSize.off, offSize.size)
+	if err != nil {
+		return "", rdr, err
+	}
+	return magic, rdr, nil
+}
+
+
+// Return the magic string defined in `r`, starting at `offset` for `size` bytes. A MultiReader is
+// wrapped around the passed in reader so that the bytes read here are not lost. Zero is returned 
+// for all errors and cases where the magic string cannot be found.
+func GetMagicString(r io.ReadCloser, offset, size int) (string, io.ReadCloser, error) {
+	bufSize := offset + size
+	buf := make([]byte, bufSize)
+	cnt, err := io.ReadFull(r, buf)
+	if err != nil && err != io.EOF {
+		return "", r, errors.Wrap(err, "unable to read byte buffer")
+	}
+	multiR := ioutil.NopCloser(io.MultiReader(bytes.NewReader(buf), r)) // don't lose bytes just read
+	if cnt < bufSize {
+		return "", multiR, errors.Errorf("only %d bytes read to find magic num at offset %d of size %d", cnt, offset, size)
+	}
+	magicStr := hex.EncodeToString(buf[offset:])
+	return magicStr, multiR, nil
 }
