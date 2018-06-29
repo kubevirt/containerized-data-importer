99145dc5357e1a56b22f31be12a85935d7c32b2e
Scott Creeley <screeley@redhat.com>
2018-06-29 09:56:54 -0400

    add unit test for image package

--- pkg/image/qemu_test.go
+++ pkg/image/qemu_test.go
@@ -1,7 +1,10 @@
 package image
 
 import (
+	"bytes"
 	"io"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 )
@@ -10,13 +13,31 @@ func TestGetMagicNumber(t *testing.T) {
 	type args struct {
 		f io.Reader
 	}
+
 	tests := []struct {
 		name    string
 		args    args
 		want    []byte
 		wantErr bool
 	}{
-	// TODO: Add test cases.
+		{
+			name:    "return valid qcow2 magic number",
+			args:    args{bytes.NewReader([]byte{'Q', 'F', 'I', 0xfb, 'T', 'H', 'I', 'S'})},
+			want:    QCOW2MagicStr,
+			wantErr: false,
+		},
+		{
+			name:    "return invalid qcow2 magic number",
+			args:    args{bytes.NewReader([]byte{'F', 'I', 0xfb, 'T', 'H', 'I', 'S'})},
+			want:    []byte{'F', 'I', 0xfb, 'T'},
+			wantErr: false,
+		},
+		{
+			name:    "empty reader",
+			args:    args{bytes.NewReader([]byte{})},
+			want:    nil,
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -41,7 +62,16 @@ func TestMatchQcow2MagicNum(t *testing.T) {
 		args args
 		want bool
 	}{
-	// TODO: Add test cases.
+		{
+			name: "does match magic number",
+			args: args{[]byte{'Q', 'F', 'I', 0xfb, 'T', 'H', 'I', 'S'}},
+			want: true,
+		},
+		{
+			name: "does not match magic number",
+			args: args{[]byte{'Q', 'T', 'I', 0xfb, 'T', 'H', 'I', 'S'}},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -57,17 +87,32 @@ func TestConvertQcow2ToRaw(t *testing.T) {
 		src  string
 		dest string
 	}
+	imageDir, _ := filepath.Abs("../../test/images")
+	goodImage := filepath.Join(imageDir, "cirros-qcow2.img")
+	badImage := filepath.Join(imageDir, "tinyCore.iso")
+	defer os.Remove("/tmp/cirros-test-good")
+	defer os.Remove("/tmp/cirros-test-bad")
+
 	tests := []struct {
 		name    string
 		args    args
 		wantErr bool
 	}{
-	// TODO: Add test cases.
+		{
+			name:    "convert qcow2 image to Raw",
+			args:    args{goodImage, filepath.Join(os.TempDir(), "cirros-test-good")},
+			wantErr: false,
+		},
+		{
+			name:    "failed to convert non qcow2 image to Raw",
+			args:    args{badImage, filepath.Join(os.TempDir(), "cirros-test-bad")},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := ConvertQcow2ToRaw(tt.args.src, tt.args.dest); (err != nil) != tt.wantErr {
-				t.Errorf("ConvertQcow2ToRaw() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("ConvertQcow2ToRaw() error = %v, wantErr %v %v", err, tt.wantErr, imageDir)
 			}
 		})
 	}
