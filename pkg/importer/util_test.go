2b0cb4cad6fc9c06a40dc3bc8481af123947374d
David Vossel <davidvossel@gmail.com>
2018-07-10 14:38:52 -0400

    fix transient failure in datavolume unit tests (#250)
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- pkg/importer/util_test.go
+++ pkg/importer/util_test.go
@@ -2,9 +2,10 @@ package importer
 
 import (
 	"io"
-	"net/url"
-	"reflect"
 	"testing"
+	"os"
+	"kubevirt.io/containerized-data-importer/pkg/common"
+	"strings"
 )
 
 func TestParseEnvVar(t *testing.T) {
@@ -12,16 +13,49 @@ func TestParseEnvVar(t *testing.T) {
 		envVarName string
 		decode     bool
 	}
+
+	os.Setenv("TESTKEY", "KEYVALUE")
+	os.Setenv("ENCODEDKEY", "RU5DT0RFRFZBTFVF")
+	os.Setenv("BADKEY", "")
+
 	tests := []struct {
-		name string
-		args args
-		want string
+		name    string
+		args    args
+		want    string
+		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:    "successfully get EnvVar",
+			args:    args{"TESTKEY", false},
+			want:    "KEYVALUE",
+			wantErr: false,
+		},
+		{
+			name:    "successfully get encoded EnvVar",
+			args:    args{"ENCODEDKEY", true},
+			want:    "ENCODEDVALUE",
+			wantErr: false,
+		},
+		{
+			name:    "invalid EnvVar",
+			args:    args{"FAKETESTKEY", false},
+			want:    "",
+			wantErr: false,
+		},
+		{
+			name:    "produce error when trying to decode non-encoded EnvVar",
+			args:    args{"BADKEY", true},
+			want:    "",
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got, _ := ParseEnvVar(tt.args.envVarName, tt.args.decode); got != tt.want {
+			got, err := ParseEnvVar(tt.args.envVarName, tt.args.decode)
+			if err != nil && !tt.wantErr {
+				t.Errorf("ParseEnvVar() do not expect error but got %v, want %v", err, tt.wantErr)
+			}
+			if got != tt.want {
 				t.Errorf("ParseEnvVar() = %v, want %v", got, tt.want)
 			}
 		})
@@ -32,23 +66,49 @@ func TestParseEndpoint(t *testing.T) {
 	type args struct {
 		endpt string
 	}
+
 	tests := []struct {
 		name    string
 		args    args
-		want    *url.URL
+		want    string
+		setEnv  bool
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:    "successfully get url object from endpoint",
+			args:    args{"http://www.bing.com"},
+			want:    "good value",
+			setEnv:  true,
+			wantErr: false,
+		},
+		{
+			name:    "successfully get url object from default value",
+			args:    args{""},
+			want:    "good value",
+			setEnv:  true,
+			wantErr: false,
+		},
+		{
+			name:    "return error with empty endpoint and no default endpoint set",
+			args:    args{""},
+			want:    "",
+			setEnv:  false,
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			os.Setenv(common.IMPORTER_ENDPOINT, "www.google.com")
+			if !tt.setEnv {
+				os.Unsetenv(common.IMPORTER_ENDPOINT)
+			}
 			got, err := ParseEndpoint(tt.args.endpt)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ParseEndpoint() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("ParseEndpoint() = %v, want %v", got, tt.want)
+			if got == nil && len(tt.want) > 0 {
+				t.Errorf("ParseEndpoint() did not get url object and expected it = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -64,10 +124,20 @@ func TestStreamDataToFile(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:    "successfully streamDataToFile",
+			args:    args{	strings.NewReader("test data for reader 1"), "/tmp/testoutfile"},
+			wantErr: false,
+		},
+		{
+			name:    "expect error when trying to open an invalid out file",
+			args:    args{	strings.NewReader("test data for reader 1"), ""},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			defer os.Remove(tt.args.filePath)
 			if err := StreamDataToFile(tt.args.dataReader, tt.args.filePath); (err != nil) != tt.wantErr {
 				t.Errorf("StreamDataToFile() error = %v, wantErr %v", err, tt.wantErr)
 			}
