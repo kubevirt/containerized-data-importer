6d02393611b63af5a8dd007239fe987af85a2a60
Alexander Wels <awels@redhat.com>
2018-08-31 10:50:49 -0400

    Update no annotation PVC import test.
    - Set verbosity on controller to 3 to enable simpler tests.
    
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- tests/import_test.go
+++ tests/import_test.go
@@ -1,19 +1,23 @@
 package tests_test
 
 import (
+	"strings"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"kubevirt.io/containerized-data-importer/pkg/controller"
+	"kubevirt.io/containerized-data-importer/tests"
 	"kubevirt.io/containerized-data-importer/tests/framework"
 	"kubevirt.io/containerized-data-importer/tests/utils"
 )
 
 const (
-	testSuiteName           = "Importer Test Suite"
-	namespacePrefix         = "importer"
-	waitForCDIImportTimeout = 30 * time.Second
+	testSuiteName                    = "Importer Test Suite"
+	namespacePrefix                  = "importer"
+	assertionPollInterval            = 2 * time.Second
+	controllerSkipPVCCompleteTimeout = 60 * time.Second
 )
 
 var _ = Describe(testSuiteName, func() {
@@ -24,9 +28,14 @@ var _ = Describe(testSuiteName, func() {
 
 	It("Should not perform CDI operations on PVC without annotations", func() {
 		pvc, err := f.CreatePVCFromDefinition(utils.NewPVCDefinition("no-import", "1G", nil, nil))
+		By("Verifying PVC with no annotation remains empty")
+		Eventually(func() bool {
+			log, err := tests.RunKubectlCommand(f, "logs", f.ControllerPod.Name, "-n", f.CdiInstallNs)
+			Expect(err).NotTo(HaveOccurred())
+			return strings.Contains(log, "pvc annotation \""+controller.AnnEndpoint+"\" not found, skipping pvc \""+f.Namespace.Name+"/no-import\"")
+		}, controllerSkipPVCCompleteTimeout, assertionPollInterval).Should(BeTrue())
 		Expect(err).ToNot(HaveOccurred())
 		// Wait a while to see if CDI puts anything in the PVC.
-		time.Sleep(waitForCDIImportTimeout)
 		Expect(framework.VerifyPVCIsEmpty(f, pvc)).To(BeTrue())
 		// Not deleting PVC as it will be removed with the NS removal.
 	})
