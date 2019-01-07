34c84d7c7d50d89eff4b42549f1302629253baf4
Alexander Wels <awels@redhat.com>
2019-01-07 16:28:25 -0500

    Update prometheus go client to latest version.
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- vendor/github.com/onsi/gomega/matchers/panic_matcher_test.go
+++ vendor/github.com/onsi/gomega/matchers/panic_matcher_test.go
@@ -10,43 +10,36 @@ var _ = Describe("Panic", func() {
 	Context("when passed something that's not a function that takes zero arguments and returns nothing", func() {
 		It("should error", func() {
 			success, err := (&PanicMatcher{}).Match("foo")
-			Expect(success).To(BeFalse())
-			Expect(err).To(HaveOccurred())
+			Ω(success).Should(BeFalse())
+			Ω(err).Should(HaveOccurred())
 
 			success, err = (&PanicMatcher{}).Match(nil)
-			Expect(success).To(BeFalse())
-			Expect(err).To(HaveOccurred())
+			Ω(success).Should(BeFalse())
+			Ω(err).Should(HaveOccurred())
 
 			success, err = (&PanicMatcher{}).Match(func(foo string) {})
-			Expect(success).To(BeFalse())
-			Expect(err).To(HaveOccurred())
+			Ω(success).Should(BeFalse())
+			Ω(err).Should(HaveOccurred())
 
 			success, err = (&PanicMatcher{}).Match(func() string { return "bar" })
-			Expect(success).To(BeFalse())
-			Expect(err).To(HaveOccurred())
+			Ω(success).Should(BeFalse())
+			Ω(err).Should(HaveOccurred())
 		})
 	})
 
 	Context("when passed a function of the correct type", func() {
 		It("should call the function and pass if the function panics", func() {
-			Expect(func() { panic("ack!") }).To(Panic())
-			Expect(func() {}).NotTo(Panic())
+			Ω(func() { panic("ack!") }).Should(Panic())
+			Ω(func() {}).ShouldNot(Panic())
 		})
 	})
 
 	Context("when assertion fails", func() {
-		It("prints the object passed to Panic when negative", func() {
+		It("should print the object passed to Panic", func() {
 			failuresMessages := InterceptGomegaFailures(func() {
-				Expect(func() { panic("ack!") }).NotTo(Panic())
+				Ω(func() { panic("ack!") }).ShouldNot(Panic())
 			})
-			Expect(failuresMessages).To(ConsistOf(ContainSubstring("not to panic, but panicked with\n    <string>: ack!")))
-		})
-
-		It("prints simple message when positive", func() {
-			failuresMessages := InterceptGomegaFailures(func() {
-				Expect(func() {}).To(Panic())
-			})
-			Expect(failuresMessages).To(ConsistOf(MatchRegexp("Expected\n\\s+<func\\(\\)>: .+\nto panic")))
+			Ω(failuresMessages).Should(ConsistOf(MatchRegexp("not to panic, but panicked with\\s*<string>: ack!")))
 		})
 	})
 })
