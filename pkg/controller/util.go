49a740d7ba67b8f4372bbdd57a008d6c9376656c
Michael Henriksen <mhenriks@redhat.com>
2019-03-04 18:48:00 -0500

    make CDIConfig client cluster scoped and fix occasional crash

--- pkg/controller/util.go
+++ pkg/controller/util.go
@@ -920,39 +920,31 @@ func MakeUploadServiceSpec(name string, pvc *v1.PersistentVolumeClaim) *v1.Servi
 	return service
 }
 
-// CreateCDIConfig creates cdi config manifest and sends to server
-func CreateCDIConfig(client kubernetes.Interface, cdiClient clientset.Interface, name string) (*cdiv1.CDIConfig, error) {
-	ns := util.GetNamespace()
-	cfg := MakeCDIConfigSpec(name)
+// EnsureCDIConfigExists creates an empty CDIConfig if necessary
+func EnsureCDIConfigExists(client kubernetes.Interface, cdiClient clientset.Interface, name string) error {
+	cfg := MakeEmptyCDIConfigSpec(name)
 
 	err := operator.SetOwner(client, cfg)
 	if err != nil {
-		return nil, errors.Wrap(err, "Error setting CDI config owner ref")
+		return errors.Wrap(err, "Error setting CDI config owner ref")
 	}
 
-	config, err := cdiClient.CdiV1alpha1().CDIConfigs(ns).Create(cfg)
+	config, err := cdiClient.CdiV1alpha1().CDIConfigs().Create(cfg)
 	if err != nil {
 		if k8serrors.IsAlreadyExists(err) {
-			config, err = cdiClient.CdiV1alpha1().CDIConfigs(ns).Update(cfg)
-			if err != nil {
-				return nil, errors.Wrap(err, "Error updating CDI Config")
-			}
-		} else {
-			return nil, errors.Wrap(err, "CDI config create errored")
+			return nil
 		}
+
+		return errors.Wrap(err, "CDI config create errored")
 	}
 
-	glog.V(1).Infof("CDI config \"%s/%s\" created\n", config.Namespace, config.Name)
-	return config, nil
+	glog.V(1).Infof("CDI config \"%s\" created\n", config.Name)
+	return nil
 }
 
-// MakeCDIConfigSpec creates cdi config manifest
-func MakeCDIConfigSpec(name string) *cdiv1.CDIConfig {
+// MakeEmptyCDIConfigSpec creates cdi config manifest
+func MakeEmptyCDIConfigSpec(name string) *cdiv1.CDIConfig {
 	config := &cdiv1.CDIConfig{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "CDIConfig",
-			APIVersion: "cdi.kubevirt.io/v1alpha1",
-		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
 			Labels: map[string]string{
@@ -960,19 +952,13 @@ func MakeCDIConfigSpec(name string) *cdiv1.CDIConfig {
 				common.CDIComponentLabel: "",
 			},
 		},
-		Spec: cdiv1.CDIConfigSpec{},
 	}
 	return config
 
 }
 
 func updateCDIConfig(cdiClient clientset.Interface, config *cdiv1.CDIConfig) error {
-	ns := config.Namespace
-	if ns == "" {
-		ns = util.GetNamespace()
-	}
-
-	_, err := cdiClient.CdiV1alpha1().CDIConfigs(ns).Update(config)
+	_, err := cdiClient.CdiV1alpha1().CDIConfigs().Update(config)
 	if err != nil {
 		return err
 	}
