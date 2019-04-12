49ebfff1b4d225e1a9e9c771e4379174656eeadf
Alexander Wels <awels@redhat.com>
2019-04-12 09:04:32 -0400

    Add prometheus progress stats for importing Reader.
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- cmd/cdi-cloner/clonertarget.go
+++ cmd/cdi-cloner/clonertarget.go
@@ -7,22 +7,15 @@ import (
 	"io/ioutil"
 	"os"
 	"strconv"
-	"time"
 
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
-	dto "github.com/prometheus/client_model/go"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/klog"
 	"kubevirt.io/containerized-data-importer/pkg/common"
 	"kubevirt.io/containerized-data-importer/pkg/util"
 	prometheusutil "kubevirt.io/containerized-data-importer/pkg/util/prometheus"
 )
-
-type prometheusProgressReader struct {
-	util.CountingReader
-	total uint64
-}
 
 const (
 	maxSizeLength = 20
@@ -87,17 +80,8 @@ func main() {
 	}
 	defer out.Close()
 
-	promReader := &prometheusProgressReader{
-		CountingReader: util.CountingReader{
-			Reader:  out,
-			Current: 0,
-		},
-		total: total,
-	}
-
-	// Start the progress update thread.
-	go promReader.timedUpdateProgress()
-
+	promReader := prometheusutil.NewProgressReader(out, total, progress, ownerUID)
+	promReader.StartTimedUpdate()
 	volumeMode := v1.PersistentVolumeBlock
 	if _, err := os.Stat(common.ImporterWriteBlockPath); os.IsNotExist(err) {
 		volumeMode = v1.PersistentVolumeFilesystem
@@ -125,26 +109,6 @@ func collectTotalSize() (uint64, error) {
 	}
 	defer out.Close()
 	return readTotal(out)
-}
-
-func (r *prometheusProgressReader) timedUpdateProgress() {
-	for true {
-		// Update every second.
-		time.Sleep(time.Second)
-		r.updateProgress()
-	}
-}
-
-func (r *prometheusProgressReader) updateProgress() {
-	if r.total > 0 {
-		currentProgress := float64(r.Current) / float64(r.total) * 100.0
-		metric := &dto.Metric{}
-		progress.WithLabelValues(ownerUID).Write(metric)
-		if currentProgress > *metric.Counter.Value {
-			progress.WithLabelValues(ownerUID).Add(currentProgress - *metric.Counter.Value)
-		}
-		klog.V(1).Infoln(fmt.Sprintf("%.2f", currentProgress))
-	}
 }
 
 // read total file size from reader, and return the value as an int64
---
+++ pkg/util/prometheus/prometheus_test.go
@@ -0,0 +1,107 @@
+package prometheus
+
+import (
+	"bytes"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"io/ioutil"
+
+	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
+	"kubevirt.io/containerized-data-importer/pkg/util"
+)
+
+var (
+	progress *prometheus.CounterVec
+	ownerUID string
+)
+
+func init() {
+	progress = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "test_progress",
+			Help: "The test progress in percentage",
+		},
+		[]string{"ownerUID"},
+	)
+	ownerUID = "1111-1111-111"
+}
+
+var _ = Describe("Timed update", func() {
+
+	It("Should start and stop when finished", func() {
+		r := ioutil.NopCloser(bytes.NewReader([]byte("hello world")))
+		progressReader := NewProgressReader(r, uint64(11), progress, ownerUID)
+		progressReader.StartTimedUpdate()
+		_, err := ioutil.ReadAll(r)
+		Expect(err).ToNot(HaveOccurred())
+	})
+})
+
+var _ = Describe("Update Progress", func() {
+	BeforeEach(func() {
+		progress = prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "test_progress",
+				Help: "The test progress in percentage",
+			},
+			[]string{"ownerUID"},
+		)
+	})
+
+	It("Parse valid progress update", func() {
+		By("Verifying the initial value is 0")
+		progress.WithLabelValues(ownerUID).Add(0)
+		metric := &dto.Metric{}
+		progress.WithLabelValues(ownerUID).Write(metric)
+		Expect(*metric.Counter.Value).To(Equal(float64(0)))
+		By("Calling updateProgress with value")
+		promReader := &ProgressReader{
+			CountingReader: util.CountingReader{
+				Current: uint64(45),
+			},
+			total:    uint64(100),
+			progress: progress,
+			ownerUID: ownerUID,
+		}
+		result := promReader.updateProgress()
+		Expect(true).To(Equal(result))
+		progress.WithLabelValues(ownerUID).Write(metric)
+		Expect(*metric.Counter.Value).To(Equal(float64(45)))
+	})
+
+	It("0 total should return 0", func() {
+		metric := &dto.Metric{}
+		By("Calling updateProgress with value")
+		promReader := &ProgressReader{
+			CountingReader: util.CountingReader{
+				Current: uint64(45),
+			},
+			total:    uint64(0),
+			progress: progress,
+			ownerUID: ownerUID,
+		}
+		result := promReader.updateProgress()
+		Expect(false).To(Equal(result))
+		progress.WithLabelValues(ownerUID).Write(metric)
+		Expect(*metric.Counter.Value).To(Equal(float64(0)))
+	})
+
+	It("current and total equals should return false", func() {
+		metric := &dto.Metric{}
+		By("Calling updateProgress with value")
+		promReader := &ProgressReader{
+			CountingReader: util.CountingReader{
+				Current: uint64(1000),
+			},
+			total:    uint64(1000),
+			progress: progress,
+			ownerUID: ownerUID,
+		}
+		result := promReader.updateProgress()
+		Expect(false).To(Equal(result))
+		progress.WithLabelValues(ownerUID).Write(metric)
+		Expect(*metric.Counter.Value).To(Equal(float64(100)))
+	})
+
+})
