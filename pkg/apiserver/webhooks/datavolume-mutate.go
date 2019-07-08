d32cb72c0b0c1f6846de92ac0785f560d9c9b1f4
Michael Henriksen <mhenriks@redhat.com>
2019-07-08 14:55:49 -0400

    make clone permission check exportable function

--- pkg/apiserver/webhooks/datavolume-mutate.go
+++ pkg/apiserver/webhooks/datavolume-mutate.go
@@ -20,17 +20,14 @@
 package webhooks
 
 import (
-	"fmt"
-
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
-	authentication "k8s.io/api/authentication/v1"
-	authorization "k8s.io/api/authorization/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8sfield "k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog"
 
 	cdiv1alpha1 "kubevirt.io/containerized-data-importer/pkg/apis/core/v1alpha1"
+	"kubevirt.io/containerized-data-importer/pkg/apiserver/webhooks/api"
 	"kubevirt.io/containerized-data-importer/pkg/controller"
 	"kubevirt.io/containerized-data-importer/pkg/token"
 )
@@ -86,7 +83,7 @@ func (wh *dataVolumeMutatingWebhook) Admit(ar admissionv1beta1.AdmissionReview) 
 		}
 	}
 
-	ok, reason, err := wh.canClonePVC(pvcSource.Namespace, pvcSource.Name, ar.Request.UserInfo)
+	ok, reason, err := api.CanClonePVC(wh.client, pvcSource.Namespace, pvcSource.Name, ar.Request.UserInfo)
 	if err != nil {
 		return toAdmissionResponseError(err)
 	}
@@ -128,46 +125,4 @@ func (wh *dataVolumeMutatingWebhook) Admit(ar admissionv1beta1.AdmissionReview) 
 	klog.V(3).Infof("Sending patch response...")
 
 	return toPatchResponse(dataVolume, modifiedDataVolume)
-}
-
-func (wh *dataVolumeMutatingWebhook) canClonePVC(namespace, name string, userInfo authentication.UserInfo) (bool, string, error) {
-	var newExtra map[string]authorization.ExtraValue
-	if len(userInfo.Extra) > 0 {
-		newExtra = make(map[string]authorization.ExtraValue)
-		for k, v := range userInfo.Extra {
-			newExtra[k] = authorization.ExtraValue(v)
-		}
-	}
-
-	sar := &authorization.SubjectAccessReview{
-		Spec: authorization.SubjectAccessReviewSpec{
-			User:   userInfo.Username,
-			Groups: userInfo.Groups,
-			Extra:  newExtra,
-			ResourceAttributes: &authorization.ResourceAttributes{
-				Namespace:   namespace,
-				Verb:        "create",
-				Group:       cdiv1alpha1.SchemeGroupVersion.Group,
-				Version:     cdiv1alpha1.SchemeGroupVersion.Version,
-				Resource:    "datavolumes",
-				Subresource: cdiv1alpha1.DataVolumeCloneSourceSubresource,
-				Name:        name,
-			},
-		},
-	}
-
-	klog.V(3).Infof("Sending SubjectAccessReview %+v", sar)
-
-	response, err := wh.client.AuthorizationV1().SubjectAccessReviews().Create(sar)
-	if err != nil {
-		return false, "", err
-	}
-
-	klog.V(3).Infof("SubjectAccessReview response %+v", response)
-
-	if !response.Status.Allowed {
-		return false, fmt.Sprintf("User %s has insufficient permissions in clone source namespace %s", userInfo.Username, namespace), nil
-	}
-
-	return true, "", nil
 }
