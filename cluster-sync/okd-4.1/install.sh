a19a7f461593b6e4728e3159e7442baa71365403
Alexander Wels <awels@redhat.com>
2019-09-05 14:15:02 -0400

    Add upgrade from release version 1.10.4 to current master. Switch OKD-4.1 provider from olm install to operator install.
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- cluster-sync/okd-4.1/install.sh
+++ cluster-sync/okd-4.1/install.sh
@@ -1,4 +1,1 @@
-CDI_INSTALL=${CDI_INSTALL_OLM}
-CDI_OLM_MANIFESTS_CATALOG_SRC="_out/manifests/release/olm/os/cdi-catalogsource-registry.yaml"
-CDI_OLM_MANIFESTS_SUBSCRIPTION="_out/manifests/release/olm/os/cdi-subscription.yaml"
-
+CDI_INSTALL=${CDI_INSTALL_OPERATOR}
