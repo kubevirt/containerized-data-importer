ce50edd3c3517840dc22791fcccb5cca4de770a2
Michael Henriksen <mhenriks@redhat.com>
2019-09-19 08:44:44 -0400

    Backport 961 (#965)
    * create aggregate cluster roles
    
    Signed-off-by: Michael Henriksen <mhenriks@redhat.com>
    
    * CDIConfig needs a ClusterRoleBinding so bind to system:authenticated for reading
    
    Signed-off-by: Michael Henriksen <mhenriks@redhat.com>
    
    * comment out problematic test
    
    Signed-off-by: Michael Henriksen <mhenriks@redhat.com>
    
    * operator needs upload.cdi.kubevirt.io permission
    
    Signed-off-by: Michael Henriksen <mhenriks@redhat.com>
    
    * revert commented out tests
    
    Signed-off-by: Michael Henriksen <mhenriks@redhat.com>

--- pkg/operator/resources/cluster/rbac.go
+++ pkg/operator/resources/cluster/rbac.go
@@ -19,6 +19,7 @@ package cluster
 import (
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 
 	"kubevirt.io/containerized-data-importer/pkg/operator/resources/utils"
 )
@@ -77,6 +78,175 @@ func createClusterRole(name string, labels map[string]string) *rbacv1.ClusterRol
 		ObjectMeta: metav1.ObjectMeta{
 			Name:   name,
 			Labels: labels,
+		},
+	}
+}
+
+func createAggregateClusterRoles(args *FactoryArgs) []runtime.Object {
+	return []runtime.Object{
+		createAggregateClusterRole("cdi.kubevirt.io:admin", "admin", getAdminPolicyRules()),
+		createAggregateClusterRole("cdi.kubevirt.io:edit", "edit", getEditPolicyRules()),
+		createAggregateClusterRole("cdi.kubevirt.io:view", "view", getViewPolicyRules()),
+		createConfigReaderClusterRole("cdi.kubevirt.io:config-reader"),
+		createConfigReaderClusterRoleBinding("cdi.kubevirt.io:config-reader"),
+	}
+}
+
+func createAggregateClusterRole(name, aggregateTo string, policyRules []rbacv1.PolicyRule) *rbacv1.ClusterRole {
+	labels := map[string]string{
+		"rbac.authorization.k8s.io/aggregate-to-" + aggregateTo: "true",
+	}
+	role := createClusterRole(name, utils.WithCommonLabels(labels))
+	role.Rules = policyRules
+	return role
+}
+
+func getAdminPolicyRules() []rbacv1.PolicyRule {
+	return []rbacv1.PolicyRule{
+		{
+			APIGroups: []string{
+				"cdi.kubevirt.io",
+			},
+			Resources: []string{
+				"datavolumes",
+			},
+			Verbs: []string{
+				"*",
+			},
+		},
+		{
+			APIGroups: []string{
+				"cdi.kubevirt.io",
+			},
+			Resources: []string{
+				"datavolumes/source",
+			},
+			Verbs: []string{
+				"create",
+			},
+		},
+		{
+			APIGroups: []string{
+				"cdi.kubevirt.io",
+			},
+			Resources: []string{
+				"cdiconfigs",
+			},
+			Verbs: []string{
+				"get",
+				"list",
+				"watch",
+				"patch",
+				"update",
+			},
+		},
+		{
+			APIGroups: []string{
+				"upload.cdi.kubevirt.io",
+			},
+			Resources: []string{
+				"uploadtokenrequests",
+			},
+			Verbs: []string{
+				"get",
+				"list",
+				"create",
+			},
+		},
+	}
+}
+
+func getEditPolicyRules() []rbacv1.PolicyRule {
+	// diff between admin and edit ClusterRoles is minimal and limited to RBAC
+	// both can CRUD pods/PVCs/etc
+	return getAdminPolicyRules()
+}
+
+func getViewPolicyRules() []rbacv1.PolicyRule {
+	return []rbacv1.PolicyRule{
+		{
+			APIGroups: []string{
+				"cdi.kubevirt.io",
+			},
+			Resources: []string{
+				"datavolumes",
+			},
+			Verbs: []string{
+				"get",
+				"list",
+				"watch",
+			},
+		},
+		{
+			APIGroups: []string{
+				"cdi.kubevirt.io",
+			},
+			Resources: []string{
+				"datavolumes/source",
+			},
+			Verbs: []string{
+				"create",
+			},
+		},
+		{
+			APIGroups: []string{
+				"cdi.kubevirt.io",
+			},
+			Resources: []string{
+				"cdiconfigs",
+			},
+			Verbs: []string{
+				"get",
+				"list",
+				"watch",
+			},
+		},
+	}
+}
+
+func createConfigReaderClusterRole(name string) *rbacv1.ClusterRole {
+	role := CreateClusterRole(name)
+
+	role.Rules = []rbacv1.PolicyRule{
+		{
+			APIGroups: []string{
+				"cdi.kubevirt.io",
+			},
+			Resources: []string{
+				"cdiconfigs",
+			},
+			Verbs: []string{
+				"get",
+				"list",
+				"watch",
+			},
+		},
+	}
+
+	return role
+}
+
+func createConfigReaderClusterRoleBinding(name string) *rbacv1.ClusterRoleBinding {
+	return &rbacv1.ClusterRoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "rbac.authorization.k8s.io/v1",
+			Kind:       "ClusterRoleBinding",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   name,
+			Labels: utils.WithCommonLabels(nil),
+		},
+		RoleRef: rbacv1.RoleRef{
+			Kind:     "ClusterRole",
+			Name:     name,
+			APIGroup: "rbac.authorization.k8s.io",
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:     "Group",
+				Name:     "system:authenticated",
+				APIGroup: "rbac.authorization.k8s.io",
+			},
 		},
 	}
 }
