49699cf53aae0e19570a32f5ebd12db777da76a5
Alexander Wels <awels@redhat.com>
2019-09-27 13:32:24 -0400

    Wait for SC to be available before making it default
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- cluster-sync/okd-4.1/provider.sh
+++ cluster-sync/okd-4.1/provider.sh
@@ -7,7 +7,22 @@ function seed_images(){
 }
 
 function configure_local_storage() {
-  #Set the default storage class.
+  retry_counter=0
+  sc=`_kubectl get sc local -o=jsonpath="{.metadata.name}"`
+  all_sc=`_kubectl get sc`
+  echo "All storage classes:"
+  echo "${all_sc}"
+  while [[ $retry_counter -lt 60 ]] && [ "$sc" != "local" ]; do
+    sc=`_kubectl get sc local -o=jsonpath="{.metadata.name}"`
+    retry_counter=$((retry_counter + 1))
+    echo "Sleep 5s, waiting for local storage class, current sc=[$sc]"
+    sleep 5
+    all_sc=`_kubectl get sc`
+    echo "All storage classes:"
+    echo "${all_sc}"
+  done
+
+  #Set the default storage class. If the above timed out, this will fail and abort the sync
   _kubectl patch storageclass local -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 }
 
