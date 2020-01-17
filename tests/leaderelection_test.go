2f61ea141b2711e477f81fa35172166e80a1d221
Alexander Wels <awels@redhat.com>
2020-01-17 14:13:47 -0500

    Don't run raw images through qemu-img as it sometimes has issues transferring files. (#1080)
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- tests/leaderelection_test.go
+++ tests/leaderelection_test.go
@@ -33,7 +33,7 @@ const (
 var (
 	logCheckLeaderRegEx  = regexp.MustCompile("Attempting to acquire leader lease")
 	logIsLeaderRegex     = regexp.MustCompile("Successfully acquired leadership lease")
-	logImporterStarting  = regexp.MustCompile("Validating image")
+	logImporterStarting  = regexp.MustCompile("Writing data...")
 	logImporterCompleted = regexp.MustCompile("\\] \\d\\d\\.\\d{1,2}")
 
 	// These are constants we want to take the pointer of
