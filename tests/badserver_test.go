93565b2c88182c8a7d083e273604f242fd15d943
maya-r <mrashish@redhat.com>
2020-05-08 01:01:19 +0300

    Implement local webserver to mimic flawed servers (for functional tests) (#1193)
    * Implement local web server to mimic flawed servers
    
    http://cdi-bad-webserver.cdi/flaky/filename will return a 503
    "unavailable" error to most requests.
    
    http://cdi-bad-webserver.cdi/forbidden-HEAD/filename will return
    a 403 "Forbidden" error on HEAD requests.
    
    In both those cases, we validate that we still eventually succeed
    in creating a datavolume.
    
    Signed-off-by: Maya Rashish <mrashish@redhat.com>
    
    * Add the imageio server to the manual test setup instructions.
    
    Signed-off-by: Maya Rashish <mrashish@redhat.com>
    
    * Add missing bad-webserver.yaml.in (forgot to git add)
    
    Signed-off-by: Maya Rashish <mrashish@redhat.com>
    
    * Make "flaky" bad-webserver reproducibly flaky.
    
    9 failures followed by a success.
    Switch to port 9090, os-3.11 disallows using privileged ports,
    and we don't have a reason to prefer port 80.
    
    Signed-off-by: Maya Rashish <mrashish@redhat.com>
    
    * Provide polarion test IDs
    
    Signed-off-by: Maya Rashish <mrashish@redhat.com>
    
    * Make flaky test server fail less frequently.
    
    The exponential backoff makes 10 retries very slow.
    
    Signed-off-by: Maya Rashish <mrashish@redhat.com>
    
    * Tolerate being installed on a namespace that isn't "cdi"
    
    Assume that we are in the same namespace as cdi-file-host, and
    use that namespace in the URL.
    
    Signed-off-by: Maya Rashish <mrashish@redhat.com>
    
    * Use AfterEach to cleanup datavolumes after test.
    
    Suggested by Alexander Wels.
    
    Signed-off-by: Maya Rashish <mrashish@redhat.com>

---
+++ tests/badserver_test.go
@@ -0,0 +1,46 @@
+package tests
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"kubevirt.io/containerized-data-importer/tests/framework"
+	"kubevirt.io/containerized-data-importer/tests/utils"
+
+	cdiv1 "kubevirt.io/containerized-data-importer/pkg/apis/core/v1alpha1"
+)
+
+var _ = Describe("Problematic server responses", func() {
+	f := framework.NewFrameworkOrDie("badserver-func-test")
+	var dataVolume *cdiv1.DataVolume
+
+	It("[rfe_id:4109][test_id:4110][crit:low][vendor:cnv-qe@redhat.com][level:component] Should succeed even if HEAD forbidden", func() {
+		badServerTinyCoreIso := "http://cdi-bad-webserver.%s:9090/forbidden-HEAD/tinyCore.iso"
+		tinyCoreIsoURL := fmt.Sprintf(badServerTinyCoreIso, f.CdiInstallNs)
+
+		dataVolume = utils.NewDataVolumeWithHTTPImport("badserver-dv", "1Gi", tinyCoreIsoURL)
+		By("creating DataVolume")
+		dataVolume, err := utils.CreateDataVolumeFromDefinition(f.CdiClient, f.Namespace.Name, dataVolume)
+		Expect(err).ToNot(HaveOccurred())
+		utils.WaitForDataVolumePhase(f.CdiClient, f.Namespace.Name, cdiv1.Succeeded, dataVolume.Name)
+	})
+
+	It("[rfe_id:4191][test_id:4193][crit:low][vendor:cnv-qe@redhat.com][level:component] Should succeed even on a flaky server", func() {
+		badServerTinyCoreIso := "http://cdi-bad-webserver.%s:9090/flaky/tinyCore.iso"
+		tinyCoreIsoURL := fmt.Sprintf(badServerTinyCoreIso, f.CdiInstallNs)
+
+		dataVolume := utils.NewDataVolumeWithHTTPImport("badserver-dv", "1Gi", tinyCoreIsoURL)
+		By("creating DataVolume")
+		dataVolume, err := utils.CreateDataVolumeFromDefinition(f.CdiClient, f.Namespace.Name, dataVolume)
+		Expect(err).ToNot(HaveOccurred())
+		utils.WaitForDataVolumePhase(f.CdiClient, f.Namespace.Name, cdiv1.Succeeded, dataVolume.Name)
+	})
+
+	AfterEach(func() {
+		By("deleting DataVolume")
+		err := utils.DeleteDataVolume(f.CdiClient, f.Namespace.Name, dataVolumeName)
+		Expect(err).ToNot(HaveOccurred())
+	})
+})
---
+++ tools/cdi-func-test-bad-webserver/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+)
+
+func failHEAD(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "HEAD" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	redirect(w, r)
+}
+
+func flaky(w http.ResponseWriter, r *http.Request) {
+	if getCounter()%4 == 3 {
+		// succeed after 10 attempts
+		redirect(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusServiceUnavailable)
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	re := regexp.MustCompile(`[^/]*$`)
+	requestedFile := re.Find([]byte(r.URL.String()))
+
+	cdiNamespace := os.Getenv("CDI_NAMESPACE")
+	redirectURL := fmt.Sprintf("http://cdi-file-host.%s/%s", cdiNamespace, requestedFile)
+	http.Redirect(w, r, redirectURL, 301)
+}
+
+func initCounter() {
+	newCounterBuf := make([]byte, binary.MaxVarintLen64)
+	_ = binary.PutUvarint(newCounterBuf, 0)
+
+	ioutil.WriteFile("state", newCounterBuf, 0644)
+}
+
+func getCounter() uint64 {
+	counterBuf, err := ioutil.ReadFile("state")
+	if err != nil {
+		panic(err)
+	}
+
+	counter, n := binary.Uvarint(counterBuf)
+	if n <= 0 {
+		counter = 0
+	}
+
+	newCounterBuf := make([]byte, binary.MaxVarintLen64)
+	_ = binary.PutUvarint(newCounterBuf, counter+1)
+	ioutil.WriteFile("state", newCounterBuf, 0644)
+
+	return counter
+}
+
+func main() {
+	initCounter()
+	http.HandleFunc("/forbidden-HEAD/", failHEAD)
+	http.HandleFunc("/flaky/", flaky)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
