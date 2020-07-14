748f9486099ef2d68877ae1125333e5d2bf0108e
Bartosz Rybacki <brybacki@redhat.com>
2020-07-14 17:07:08 +0200

    Create a featuregates package
    Signed-off-by: Bartosz Rybacki <brybacki@redhat.com>

--- pkg/feature-gates/feature-gates.go
+++ pkg/feature-gates/feature-gates.go
@@ -1,4 +1,4 @@
-package controller
+package featuregates
 
 import (
 	"context"
--- pkg/feature-gates/feature-gates_test.go
+++ pkg/feature-gates/feature-gates_test.go
@@ -14,13 +14,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package controller
+package featuregates
 
 import (
 	"context"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
+	"kubevirt.io/containerized-data-importer/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 
@@ -64,7 +65,7 @@ func createFeatureGates(objects ...runtime.Object) *FeatureGates {
 	objs = append(objs, objects...)
 
 	// Append empty CDIConfig object that normally is created by the reconcile loop
-	cdiConfig := MakeEmptyCDIConfigSpec(common.ConfigName)
+	cdiConfig := controller.MakeEmptyCDIConfigSpec(common.ConfigName)
 	objs = append(objs, cdiConfig)
 
 	// Register operator types with the runtime scheme.
