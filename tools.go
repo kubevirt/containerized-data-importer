4aa3fdf68543c04bb064602aada9920cc716cf96
Michael Henriksen <mhenriks@redhat.com>
2020-08-06 11:38:41 -0400

    redo code generator dep management
    Signed-off-by: Michael Henriksen <mhenriks@redhat.com>

--- tools.go
+++ tools.go
@@ -1,7 +1,7 @@
-// +build tools
-
 package tools
 
 import (
+	// VSCODE will say this is an error, but it's fine
+	// the puropse of this import is to get gomod to bring in code generator and deps
 	_ "k8s.io/code-generator"
 )
