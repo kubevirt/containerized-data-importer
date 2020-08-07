0b41f8efc4e645a154d69123922d1a4c50116074
Alexander Wels <awels@redhat.com>
2020-08-07 09:30:29 -0400

    Update builder image to official latest
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- hack/build/config.sh
+++ hack/build/config.sh
@@ -27,7 +27,7 @@ FUNC_TEST_REGISTRY_INIT="cdi-func-test-registry-init"
 FUNC_TEST_BAD_WEBSERVER="cdi-func-test-bad-webserver"
 # update this whenever builder Dockerfile is updated
 BUILDER_TAG=${BUILDER_TAG:-0.0.7}
-BUILDER_IMAGE=${BUILDER_IMAGE:-mhenriks/kubevirt-cdi-bazel-builder:0.0.7}
+BUILDER_IMAGE=${BUILDER_IMAGE:-kubevirt/kubevirt-cdi-bazel-builder@sha256:9d0f12fbaec87061fe570af16df64706410c401b7a87fb48a65d814e357c4d12}
 
 BINARIES="cmd/${OPERATOR} cmd/${CONTROLLER} cmd/${IMPORTER} cmd/${CLONER} cmd/${APISERVER} cmd/${UPLOADPROXY} cmd/${UPLOADSERVER} cmd/${OPERATOR} tools/${FUNC_TEST_INIT} tools/${FUNC_TEST_REGISTRY_INIT} tools/${FUNC_TEST_BAD_WEBSERVER}"
 CDI_PKGS="cmd/ pkg/ test/"
