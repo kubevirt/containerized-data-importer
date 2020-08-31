fd340001a9647d81bc8ad1f1cea9f889424a7083
Matthew Arnold <marnold@redhat.com>
2020-08-31 13:31:37 -0400

    Read a raw image for VDDK test plugin.
    Have the fake VDDK test plugin for nbdkit read a raw image passed in
    /opt/updates/nbdtest.img instead of returning fixed data.
    
    Signed-off-by: Matthew Arnold <marnold@redhat.com>

--- tools/vddk-test/vddk-test-plugin.c
+++ tools/vddk-test/vddk-test-plugin.c
@@ -12,10 +12,17 @@
 
 #define NBDKIT_API_VERSION 2
 #include <nbdkit-plugin.h>
+#include <fcntl.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define THREAD_MODEL NBDKIT_THREAD_MODEL_SERIALIZE_ALL_REQUESTS
-#define LIMIT 524288000
+
+void fakevddk_close(void *handle) {
+    close(*((int *) handle));
+}
 
 int fakevddk_config(const char *key, const char *value) {
     return 0;
@@ -26,15 +33,41 @@ int fakevddk_config_complete(void) {
 }
 
 void *fakevddk_open(int readonly) {
-    return NBDKIT_HANDLE_NOT_NEEDED;
+    static int fd;
+    fd = open("/opt/updates/nbdtest.img", O_RDONLY);
+    if (fd == -1) {
+        nbdkit_error("Failed to open /opt/updates/nbdtest.img: %m");
+        return NULL;
+    }
+    return (void *) &fd;
 }
 
 int64_t fakevddk_get_size(void *handle) {
-    return (int64_t) LIMIT;
+    struct stat info;
+    fstat(*((int *) handle), &info);
+    return info.st_size;
 }
 
 int fakevddk_pread(void *handle, void *buf, uint32_t count, uint64_t offset, uint32_t flags) {
-    memset(buf, 0x55, count);
+    uint64_t read_offset = offset;
+    uint32_t read_count = count;
+    void *read_buffer = buf;
+
+    while (read_count > 0) {
+        ssize_t r = pread(*((int *)handle), read_buffer, read_count, read_offset);
+        if (r == 0) {
+            nbdkit_error("End-of-file from pread!");
+            return -1;
+        } else if (r == -1) {
+            nbdkit_error("Error from pread: %m");
+            return -1;
+        } else {
+            read_buffer += r;
+            read_offset += r;
+            read_count -= r;
+        }
+    }
+
     return 0;
 }
 
@@ -42,6 +75,7 @@ struct nbdkit_plugin fakevddk = {
     .name            = "vddk",
     .longname        = "Simulated VDDK plugin for CDI testing",
     .version         = "N/A",
+    .close           = fakevddk_close,
     .config          = fakevddk_config,
     .config_complete = fakevddk_config_complete,
     .open            = fakevddk_open,
