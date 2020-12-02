ab26978d90e6f6ee553cc22cf42955a367756dd1
Michael Henriksen <mhenriks@redhat.com>
2020-12-02 16:12:11 -0500

    Clone source program calls tar instead of getting piped input.  This ensures we trap tar errors.
    Fix ready/complet check in clone controller
    
    Signed-off-by: Michael Henriksen <mhenriks@redhat.com>

--- cmd/cdi-cloner/clone-source.go
+++ cmd/cdi-cloner/clone-source.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 
 	"github.com/golang/snappy"
 	"github.com/prometheus/client_golang/prometheus"
@@ -20,12 +21,35 @@ import (
 
 var (
 	contentType string
+	mountPoint  string
 	uploadBytes uint64
 )
 
+type execReader struct {
+	cmd    *exec.Cmd
+	stdout io.ReadCloser
+	stderr io.ReadCloser
+}
+
+func (er *execReader) Read(p []byte) (n int, err error) {
+	n, err = er.stdout.Read(p)
+	if err == io.EOF {
+		if err2 := er.cmd.Wait(); err2 != nil {
+			errBytes, _ := ioutil.ReadAll(er.stderr)
+			klog.Fatalf("Subprocess did not execute successfully, result is: %q\n%s", er.cmd.ProcessState.ExitCode(), string(errBytes))
+		}
+	}
+	return
+}
+
+func (er *execReader) Close() error {
+	return er.stdout.Close()
+}
+
 func init() {
-	flag.StringVar(&contentType, "content_type", "", "archive|kubevirt")
-	flag.Uint64Var(&uploadBytes, "upload_bytes", 0, "approx number of bytes in input")
+	flag.StringVar(&contentType, "content-type", "", "filesystem-clone|blockdevice-clone")
+	flag.StringVar(&mountPoint, "mount", "", "pvc mount point")
+	flag.Uint64Var(&uploadBytes, "upload-bytes", 0, "approx number of bytes in input")
 	klog.InitFlags(nil)
 }
 
@@ -104,12 +128,68 @@ func pipeToSnappy(reader io.ReadCloser) io.ReadCloser {
 	return pr
 }
 
+func validateContentType() {
+	switch contentType {
+	case "filesystem-clone", "blockdevice-clone":
+	default:
+		klog.Fatalf("Invalid content-type %q", contentType)
+	}
+}
+
+func validateMount() {
+	if mountPoint == "" {
+		klog.Fatalf("Invalid mount %q", mountPoint)
+	}
+}
+
+func newTarReader() (io.ReadCloser, error) {
+	cmd := exec.Command("/usr/bin/tar", "Scv", ".")
+	cmd.Dir = mountPoint
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
+	if err = cmd.Start(); err != nil {
+		return nil, err
+	}
+
+	return &execReader{cmd: cmd, stdout: stdout, stderr: ioutil.NopCloser(&stderr)}, nil
+}
+
+func getInputStream() (rc io.ReadCloser) {
+	var err error
+	switch contentType {
+	case "filesystem-clone":
+		rc, err = newTarReader()
+		if err != nil {
+			klog.Fatalf("Error creating tar reader for %q: %+v", mountPoint, err)
+		}
+	case "blockdevice-clone":
+		rc, err = os.Open(mountPoint)
+		if err != nil {
+			klog.Fatalf("Error opening block device %q: %+v", mountPoint, err)
+		}
+	default:
+		klog.Fatalf("Invalid content-type %q", contentType)
+	}
+	return
+}
+
 func main() {
 	flag.Parse()
 	defer klog.Flush()
 
-	klog.Infof("content_type is %q\n", contentType)
-	klog.Infof("upload_bytes is %d", uploadBytes)
+	klog.Infof("content-type is %q\n", contentType)
+	klog.Infof("mount is %q\n", mountPoint)
+	klog.Infof("upload-bytes is %d", uploadBytes)
+
+	validateContentType()
+	validateMount()
 
 	ownerUID := getEnvVarOrDie(common.OwnerUID)
 
@@ -121,7 +201,7 @@ func main() {
 
 	klog.V(1).Infoln("Starting cloner target")
 
-	reader := pipeToSnappy(createProgressReader(os.Stdin, ownerUID, uploadBytes))
+	reader := pipeToSnappy(createProgressReader(getInputStream(), ownerUID, uploadBytes))
 
 	startPrometheus()
 
