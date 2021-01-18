992b1c586a27290e55a1da2d2c6cc0bf973bf6ba
Alexander Wels <awels@redhat.com>
2021-01-18 11:20:46 -0500

    Skip test 2555 if running on openshift (#1572) (#1581)
    * Move "isOpenshift" to utils and make public.
    
    No functional change.
    
    Signed-off-by: Maya Rashish <mrashish@redhat.com>
    
    * Skip malformed too large qcow2 on openshift.
    
    Whether this test fails depends on the qemu-img version. The way we
    build and test it in this repo is fine, but external builds may fail.
    
    Skipping only on OpenShift means we will continue testing it and
    finding regressions.
    
    Signed-off-by: Maya Rashish <mrashish@redhat.com>
    
    Co-authored-by: Maya Rashish <mrashish@redhat.com>

--- tests/utils.go
+++ tests/utils.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 	"os/exec"
 	"reflect"
@@ -17,6 +18,7 @@ import (
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 
 	"kubevirt.io/containerized-data-importer/tests/framework"
 )
@@ -168,4 +170,33 @@ func PodSpecHasTestNodePlacementValues(f *framework.Framework, podSpec v1.PodSpe
 		return false
 	}
 	return true
+}
+
+//IsOpenshift checks if we are on OpenShift platform
+func IsOpenshift(client kubernetes.Interface) bool {
+	//OpenShift 3.X check
+	result := client.Discovery().RESTClient().Get().AbsPath("/oapi/v1").Do(context.TODO())
+	var statusCode int
+	result.StatusCode(&statusCode)
+
+	if result.Error() == nil {
+		// It is OpenShift
+		if statusCode == http.StatusOK {
+			return true
+		}
+	} else {
+		// Got 404 so this is not Openshift 3.X, let's check OpenShift 4
+		result = client.Discovery().RESTClient().Get().AbsPath("/apis/route.openshift.io").Do(context.TODO())
+		var statusCode int
+		result.StatusCode(&statusCode)
+
+		if result.Error() == nil {
+			// It is OpenShift
+			if statusCode == http.StatusOK {
+				return true
+			}
+		}
+	}
+
+	return false
 }
