16b9848627989ab57da54fe5183968b0cdb06515
Marcelo Amaral <marcelo.amaral1@ibm.com>
2021-02-05 21:10:31 +0900

    test: decrease the import pod log pooling interval and increase the image size to be able to verify the import pod before it is deleted
    Signed-off-by: Marcelo Amaral <marcelo.amaral1@ibm.com>

--- tests/import_proxy_test.go
+++ tests/import_proxy_test.go
@@ -16,7 +16,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/wait"
 
 	"kubevirt.io/containerized-data-importer/pkg/common"
 	"kubevirt.io/containerized-data-importer/pkg/controller"
@@ -32,166 +31,142 @@ const (
 	httpPort         = "8080"
 	httpPortWithAuth = "8081"
 	proxyServerName  = "cdi-test-proxy"
+	fileHostName     = "cdi-file-host"
+	//we use the a large image to "slowdown" the download process to have time to inspect the importer pod
+	cirrosFileName = "cirros-large-virtual-size.qcow2"
 )
 
-var _ = Describe("Import Proxy tests", func() {
-
+var _ = FDescribe("Import Proxy tests", func() {
+	type importProxyTestArguments struct {
+		name          string
+		size          string
+		url           func(bool) string
+		proxyURL      func(bool, bool) string
+		noProxy       string
+		isHTTPS       bool
+		withBasicAuth bool
+		dvFunc        func(string, string, string) *cdiv1.DataVolume
+		expected      func() types.GomegaMatcher
+	}
 	f := framework.NewFramework("import-proxy-func-test")
-	tinyCoreIsoHTTP := func() string { return fmt.Sprintf(utils.TinyCoreIsoURL, f.CdiInstallNs) }
-	tinyCoreIsoHTTPS := func() string { return fmt.Sprintf(utils.HTTPSTinyCoreIsoURL, f.CdiInstallNs) }
-	createHTTPDataVolume := func(dataVolumeName, size, url string) *cdiv1.DataVolume {
-		dataVolume := utils.NewDataVolumeWithHTTPImport(dataVolumeName, size, url)
-		return dataVolume
-	}
-	createHTTPDataVolumeWithAuth := func(dataVolumeName, size, url string) *cdiv1.DataVolume {
-		dataVolume := utils.NewDataVolumeWithHTTPImport(dataVolumeName, size, url)
-		stringData := map[string]string{
-			common.KeyAccess: username,
-			common.KeySecret: password,
-		}
-		s, _ := utils.CreateSecretFromDefinition(f.K8sClient, utils.NewSecretDefinition(nil, stringData, nil, f.Namespace.Name, "mysecret"))
-		dataVolume.Spec.Source.HTTP.SecretRef = s.Name
-		return dataVolume
-	}
-	createHTTPSDataVolume := func(dataVolumeName, size, url string) *cdiv1.DataVolume {
-		dataVolume := utils.NewDataVolumeWithHTTPImport(dataVolumeName, size, url)
-		cm, err := utils.CopyFileHostCertConfigMap(f.K8sClient, f.Namespace.Name, f.CdiInstallNs)
-		Expect(err).To(BeNil())
-		dataVolume.Spec.Source.HTTP.CertConfigMap = cm
-		return dataVolume
-	}
-	createHTTPSDataVolumeWithAuth := func(dataVolumeName, size, url string) *cdiv1.DataVolume {
-		dataVolume := utils.NewDataVolumeWithHTTPImport(dataVolumeName, size, url)
-		cm, err := utils.CopyFileHostCertConfigMap(f.K8sClient, f.Namespace.Name, f.CdiInstallNs)
-		Expect(err).To(BeNil())
-		dataVolume.Spec.Source.HTTP.CertConfigMap = cm
-		stringData := map[string]string{
-			common.KeyAccess: username,
-			common.KeySecret: password,
-		}
-		s, _ := utils.CreateSecretFromDefinition(f.K8sClient, utils.NewSecretDefinition(nil, stringData, nil, f.Namespace.Name, "mysecret"))
-		dataVolume.Spec.Source.HTTP.SecretRef = s.Name
-		return dataVolume
-	}
-	createProxyURL := func(isHTTPS bool, withBasicAuth bool) string {
-		var auth string
-		port := httpPort
-		if withBasicAuth {
-			port = httpPortWithAuth
-			auth = fmt.Sprintf("%s:%s@", username, password)
-		}
-		return fmt.Sprintf("http://%s%s.%s:%s", auth, proxyServerName, f.CdiInstallNs, port)
-	}
 
-	Describe("importing from cdi proxied web server", func() {
-		type importProxyTestArguments struct {
-			name               string
-			size               string
-			url                func() string
-			proxyURL           func(bool, bool) string
-			isHTTPS            bool
-			proxyWithBasicAuth bool
-			noProxyDomains     string
-			dvFunc             func(string, string, string) *cdiv1.DataVolume
-			expected           func() types.GomegaMatcher
+	FDescribeTable("should", func(args importProxyTestArguments) {
+		var proxyHTTPURL string
+		var proxyHTTPSURL string
+		if args.isHTTPS {
+			proxyHTTPSURL = createProxyURL(args.isHTTPS, args.withBasicAuth, f.CdiInstallNs)
+		} else {
+			proxyHTTPURL = createProxyURL(args.isHTTPS, args.withBasicAuth, f.CdiInstallNs)
 		}
+		noProxy := args.noProxy
+		imgURL := createCirrosQcowURL(args.isHTTPS, f.CdiInstallNs)
 
-		DescribeTable("should", func(args importProxyTestArguments) {
-			var proxyHTTPURL string
-			var proxyHTTPSURL string
-			if args.isHTTPS {
-				proxyHTTPSURL = args.proxyURL(args.isHTTPS, args.proxyWithBasicAuth)
-			} else {
-				proxyHTTPURL = args.proxyURL(args.isHTTPS, args.proxyWithBasicAuth)
+		By("Updating CDIConfig with ImportProxy configuration")
+		updateCDIConfigImporterProxyConfig(f, proxyHTTPURL, proxyHTTPSURL, noProxy)
+
+		By(fmt.Sprintf("Creating new datavolume %s", args.name))
+		dv := createHTTPDataVolume(f, args.name, args.size, imgURL, args.isHTTPS, args.withBasicAuth)
+		dataVolume, err := utils.CreateDataVolumeFromDefinition(f.CdiClient, f.Namespace.Name, dv)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Verifying pvc was created")
+		pvc, err := utils.WaitForPVC(f.K8sClient, dataVolume.Namespace, dataVolume.Name)
+		Expect(err).ToNot(HaveOccurred())
+		f.ForceBindIfWaitForFirstConsumer(pvc)
+
+		By("Verifying a message was printed to indicate a request was done using proxy")
+		verifyImporterPodInfoInProxyLogs(f, dataVolume, imgURL, args.isHTTPS, args.expected)
+
+		By("Waiting for the img import to be completed")
+		err = utils.WaitForDataVolumePhase(f.CdiClient, f.Namespace.Name, cdiv1.Succeeded, dataVolume.Name)
+		Expect(err).ToNot(HaveOccurred(), "Datavolume not in phase succeeded in time")
+
+		By("Deleting DataVolume")
+		err = utils.DeleteDataVolume(f.CdiClient, f.Namespace.Name, args.name)
+		Expect(err).ToNot(HaveOccurred())
+		Eventually(func() bool {
+			_, err := f.K8sClient.CoreV1().PersistentVolumeClaims(f.Namespace.Name).Get(context.TODO(), dataVolume.Name, metav1.GetOptions{})
+			if k8serrors.IsNotFound(err) {
+				return true
 			}
-			noProxy := args.noProxyDomains
+			return false
+		}, timeout, pollingInterval).Should(BeTrue())
 
-			By("Updating CDIConfig with ImportProxy configuration")
-			updateCDIConfigImporterProxyConfig(f, proxyHTTPURL, proxyHTTPSURL, noProxy)
-
-			By(fmt.Sprintf("Creating new datavolume %s", args.name))
-			dv := args.dvFunc(args.name, args.size, args.url())
-			dataVolume, err := utils.CreateDataVolumeFromDefinition(f.CdiClient, f.Namespace.Name, dv)
-			Expect(err).ToNot(HaveOccurred())
-
-			By("Verifying pvc was created")
-			pvc, err := utils.WaitForPVC(f.K8sClient, dataVolume.Namespace, dataVolume.Name)
-			Expect(err).ToNot(HaveOccurred())
-			f.ForceBindIfWaitForFirstConsumer(pvc)
-
-			By("Verifying a message was printed to indicate a request was done using proxy")
-			verifyImporterPodInfoInProxyLogs(f, dataVolume, args.isHTTPS, args.url(), args.expected)
-
-			By("Waiting for the img import to be completed")
-			err = utils.WaitForDataVolumePhase(f.CdiClient, f.Namespace.Name, cdiv1.Succeeded, dataVolume.Name)
-			Expect(err).ToNot(HaveOccurred(), "Datavolume not in phase succeeded in time")
-
-			By("Deleting DataVolume")
-			err = utils.DeleteDataVolume(f.CdiClient, f.Namespace.Name, args.name)
-			Expect(err).ToNot(HaveOccurred())
-			Eventually(func() bool {
-				_, err := f.K8sClient.CoreV1().PersistentVolumeClaims(f.Namespace.Name).Get(context.TODO(), dataVolume.Name, metav1.GetOptions{})
-				if k8serrors.IsNotFound(err) {
-					return true
-				}
-				return false
-			}, timeout, pollingInterval).Should(BeTrue())
-
-			cleanClusterWideProxy(f)
-		},
-			Entry("succeed creating import dv with a proxied server (http)", importProxyTestArguments{
-				name:               "dv-http-import",
-				size:               "1Gi",
-				url:                tinyCoreIsoHTTP,
-				proxyURL:           createProxyURL,
-				isHTTPS:            false,
-				proxyWithBasicAuth: false,
-				noProxyDomains:     "",
-				dvFunc:             createHTTPDataVolume,
-				expected:           BeTrue}),
-			Entry("succeed creating import dv with a proxied server (http) with basic autentication", importProxyTestArguments{
-				name:               "dv-http-import",
-				size:               "1Gi",
-				url:                tinyCoreIsoHTTP,
-				proxyURL:           createProxyURL,
-				isHTTPS:            false,
-				proxyWithBasicAuth: true,
-				noProxyDomains:     "",
-				dvFunc:             createHTTPDataVolumeWithAuth,
-				expected:           BeTrue}),
-			Entry("succeed creating import dv with a proxied server (https) with the target server with tls", importProxyTestArguments{
-				name:               "dv-https-import",
-				size:               "1Gi",
-				url:                tinyCoreIsoHTTPS,
-				proxyURL:           createProxyURL,
-				isHTTPS:            true,
-				proxyWithBasicAuth: false,
-				noProxyDomains:     "",
-				dvFunc:             createHTTPSDataVolume,
-				expected:           BeTrue}),
-			Entry("succeed creating import dv with a proxied server (https) with basic autentication and the target server with tls", importProxyTestArguments{
-				name:               "dv-https-import",
-				size:               "1Gi",
-				url:                tinyCoreIsoHTTPS,
-				proxyURL:           createProxyURL,
-				isHTTPS:            true,
-				proxyWithBasicAuth: true,
-				noProxyDomains:     "",
-				dvFunc:             createHTTPSDataVolumeWithAuth,
-				expected:           BeTrue}),
-			Entry("succeed creating import dv with a proxied server (https) but bypassing the proxy", importProxyTestArguments{
-				name:               "dv-https-import",
-				size:               "1Gi",
-				url:                tinyCoreIsoHTTPS,
-				proxyURL:           createProxyURL,
-				isHTTPS:            true,
-				proxyWithBasicAuth: false,
-				noProxyDomains:     "*",
-				dvFunc:             createHTTPSDataVolumeWithAuth,
-				expected:           BeFalse}),
-		)
-	})
+		cleanClusterWideProxy(f)
+	},
+		FEntry("succeed creating import dv with a proxied server (http)", importProxyTestArguments{
+			name:          "dv-import-http-proxy",
+			size:          "2.5Gi",
+			noProxy:       "",
+			isHTTPS:       false,
+			withBasicAuth: false,
+			expected:      BeTrue}),
+		FEntry("succeed creating import dv with a proxied server (http) with basic autentication", importProxyTestArguments{
+			name:          "dv-import-http-proxy-auth",
+			size:          "2.5Gi",
+			noProxy:       "",
+			isHTTPS:       false,
+			withBasicAuth: true,
+			expected:      BeTrue}),
+		FEntry("succeed creating import dv with a proxied server (https) with the target server with tls", importProxyTestArguments{
+			name:          "dv-import-https-proxy",
+			size:          "2.5Gi",
+			noProxy:       "",
+			isHTTPS:       true,
+			withBasicAuth: false,
+			expected:      BeTrue}),
+		FEntry("succeed creating import dv with a proxied server (https) with basic autentication and the target server with tls", importProxyTestArguments{
+			name:          "dv-import-https-proxy-auth",
+			size:          "2.5Gi",
+			noProxy:       "",
+			isHTTPS:       true,
+			withBasicAuth: true,
+			expected:      BeTrue}),
+		FEntry("succeed creating import dv with a proxied server (http) but bypassing the proxy", importProxyTestArguments{
+			name:          "dv-import-noproxy",
+			size:          "2.5Gi",
+			noProxy:       "*",
+			isHTTPS:       false,
+			withBasicAuth: false,
+			expected:      BeFalse}),
+	)
 })
+
+func createProxyURL(isHTTPS, withBasicAuth bool, namespace string) string {
+	var auth string
+	port := httpPort
+	if withBasicAuth {
+		port = httpPortWithAuth
+		auth = fmt.Sprintf("%s:%s@", username, password)
+	}
+	return fmt.Sprintf("http://%s%s.%s:%s", auth, proxyServerName, namespace, port)
+}
+
+func createCirrosQcowURL(withHTTPS bool, namespace string) string {
+	protocol := "http"
+	if withHTTPS {
+		protocol = "https"
+	}
+	return fmt.Sprintf("%s://%s.%s/%s", protocol, fileHostName, namespace, cirrosFileName)
+}
+
+func createHTTPDataVolume(f *framework.Framework, dataVolumeName, size, url string, isHTTPS, withBasicAuth bool) *cdiv1.DataVolume {
+	dataVolume := utils.NewDataVolumeWithHTTPImport(dataVolumeName, size, url)
+	if isHTTPS {
+		cm, err := utils.CopyFileHostCertConfigMap(f.K8sClient, f.Namespace.Name, f.CdiInstallNs)
+		Expect(err).To(BeNil())
+		dataVolume.Spec.Source.HTTP.CertConfigMap = cm
+	}
+	if withBasicAuth {
+		stringData := map[string]string{
+			common.KeyAccess: username,
+			common.KeySecret: password,
+		}
+		s, _ := utils.CreateSecretFromDefinition(f.K8sClient, utils.NewSecretDefinition(nil, stringData, nil, f.Namespace.Name, "mysecret"))
+		dataVolume.Spec.Source.HTTP.SecretRef = s.Name
+	}
+	return dataVolume
+}
 
 func updateCDIConfigImporterProxyConfig(f *framework.Framework, proxyHTTPURL string, proxyHTTPSURL string, noProxy string) {
 	if !utils.IsOpenshift(f.K8sClient) {
@@ -251,7 +226,7 @@ func updateCDIConfigByUpdatingTheClusterWideProxy(f *framework.Framework, proxyH
 		http, _ := controller.GetImportProxyConfig(config, common.ImportProxyHTTP)
 		https, _ := controller.GetImportProxyConfig(config, common.ImportProxyHTTPS)
 		if http == proxy.Status.HTTPProxy && https == proxy.Status.HTTPSProxy {
-			// enable proxing the importer pod http or https calls
+			// update the noProxy in the CDIConfig
 			err := utils.UpdateCDIConfig(f.CrClient, func(config *cdiv1.CDIConfigSpec) {
 				config.ImportProxy = &cdiv1.ImportProxy{
 					HTTPProxy:  &http,
@@ -267,68 +242,52 @@ func updateCDIConfigByUpdatingTheClusterWideProxy(f *framework.Framework, proxyH
 }
 
 // verifyImporterPodInfoInProxyLogs verifiy if the importer pod request (method, url and impoter pod IP) appears in the proxy log
-func verifyImporterPodInfoInProxyLogs(f *framework.Framework, dataVolume *cdiv1.DataVolume, isHTTPS bool, uri string, expected func() types.GomegaMatcher) {
+func verifyImporterPodInfoInProxyLogs(f *framework.Framework, dataVolume *cdiv1.DataVolume, imgURL string, isHTTPS bool, expected func() types.GomegaMatcher) {
+	podIP := getImporterPodIP(f)
 	Eventually(func() bool {
-		importerPod := getImporterPod(f)
-		proxyLog := getProxyLog(f)
-		return wasPodProxied(isHTTPS, uri, importerPod, proxyLog)
+		proxyLOG := getProxyLog(f)
+		return wasPodProxied(imgURL, podIP, proxyLOG, isHTTPS)
 	}, time.Second*60, time.Second).Should(expected())
 }
 
-func getImporterPod(f *framework.Framework) *corev1.Pod {
-	fmt.Fprintf(GinkgoWriter, "INFO: Waiting importer pod to become ready\n")
-	err := waitForPodIP(f, common.ImporterPodName, f.Namespace.Name, utils.PodWaitForTime)
-	importerPod, err := utils.FindPodByPrefix(f.K8sClient, f.Namespace.Name, common.ImporterPodName, common.CDILabelSelector)
-	Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("Unable to get importer pod"))
-	return importerPod
-}
-
-func waitForPodIP(f *framework.Framework, podName, namespace string, timeout time.Duration) error {
-	return wait.PollImmediate(2*time.Second, timeout, podIP(f, podName, namespace))
-}
-
-func podIP(f *framework.Framework, podName, namespace string) wait.ConditionFunc {
-	return func() (bool, error) {
-		pod, err := utils.FindPodByPrefix(f.K8sClient, namespace, podName, common.CDILabelSelector)
-		if err != nil {
-			if k8serrors.IsNotFound(err) {
-				return false, nil
-			}
-			return false, err
+func getImporterPodIP(f *framework.Framework) string {
+	podIP := ""
+	Eventually(func() string {
+		pod, err := utils.FindPodByPrefix(f.K8sClient, f.Namespace.Name, common.ImporterPodName, common.CDILabelSelector)
+		if err != nil || pod.Status.Phase == corev1.PodPending {
+			return ""
 		}
-		fmt.Fprintf(GinkgoWriter, "INFO: Checking POD %s phase: %s\n", podName, string(pod.Status.Phase))
-		if pod.Status.PodIP != "" {
-			return true, nil
-		}
-		return false, nil
-	}
+		fmt.Fprintf(GinkgoWriter, "INFO: Checking POD %s IP: %s\n", pod.Name, pod.Status.PodIP)
+		podIP = pod.Status.PodIP
+		return podIP
+	}, time.Second*60, time.Millisecond*100).Should(Not(BeEmpty()))
+	return podIP
 }
 
 func getProxyLog(f *framework.Framework) string {
 	proxyPod, err := utils.FindPodByPrefix(f.K8sClient, f.CdiInstallNs, proxyServerName, fmt.Sprintf("name=%s", proxyServerName))
 	Expect(err).ToNot(HaveOccurred())
 	fmt.Fprintf(GinkgoWriter, "INFO: Analyzing the proxy pod %s logs\n", proxyPod.Name)
-	log, err := RunKubectlCommand(f, "logs", proxyPod.Name, "-n", f.CdiInstallNs)
+	log, err := RunKubectlCommand(f, "logs", proxyPod.Name, "-n", proxyPod.Namespace, "--since=15m")
 	Expect(err).To(BeNil())
 	return log
 }
 
-func wasPodProxied(isHTTPS bool, uri string, importerPod *corev1.Pod, proxyLog string) bool {
-	//http method substring that will appear in the proxy log
+func wasPodProxied(imgURL, podIP, proxyLog string, isHTTPS bool) bool {
 	method := "METHOD:GET"
 	if isHTTPS {
-		//https method substring that will appear in the proxy log. Since the proxy hijacks the HTTPS conections (i.e., tunneling), the first https request uses the method "CONNECT" instead of "GET"
+		//Since the proxy hijacks the HTTPS conections (i.e., tunneling), the first https request uses the method "CONNECT" instead of "GET"
 		method = "METHOD:CONNECT"
 	}
-	u, _ := url.Parse(uri)
+	u, _ := url.Parse(imgURL)
 	for _, line := range strings.Split(strings.TrimSuffix(proxyLog, "\n"), "\n") {
-		if strings.Contains(line, method) && strings.Contains(line, u.Host) && strings.Contains(line, importerPod.Status.PodIP) {
+		if strings.Contains(line, method) && strings.Contains(line, u.Host) && strings.Contains(line, podIP) {
 			fmt.Fprintf(GinkgoWriter, "INFO: Proxy log: %s\n", line)
-			fmt.Fprintf(GinkgoWriter, "INFO: The import POD %s requests were proxied\n", importerPod.Name)
+			fmt.Fprintf(GinkgoWriter, "INFO: The import POD requests were proxied\n")
 			return true
 		}
 	}
-	fmt.Fprintf(GinkgoWriter, "INFO: The import POD %s requests were not proxied\n", importerPod.Name)
+	fmt.Fprintf(GinkgoWriter, "INFO: The import POD requests were not proxied\n")
 	return false
 }
 
