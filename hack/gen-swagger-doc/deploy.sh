1e169b2b789cf928790950c68bdb59742286d2e5
Alexander Wels <awels@redhat.com>
2021-03-11 08:29:03 -0500

    Add scripts to do releases with PROW
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- hack/gen-swagger-doc/deploy.sh
+++ hack/gen-swagger-doc/deploy.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-set -e
+set -exo pipefail
 
 GITHUB_FQDN=github.com
-API_REF_REPO=kubevirt/cdi-api-reference
+API_REF_REPO=${API_REF_REPO:-kubevirt/cdi-api-reference}
 API_REF_DIR=/tmp/cdi-api-reference
 GITHUB_IO_FQDN="https://kubevirt.io/cdi-api-reference"
 
-TARGET_DIR="$TRAVIS_BRANCH"
-if [ -n "${TRAVIS_TAG}" ]; then
-    TARGET_DIR="$TRAVIS_TAG"
+TARGET_DIR="$PULL_BASE_REF"
+if [ -n "${DOCKER_TAG}" ]; then
+    TARGET_DIR="$DOCKER_TAG"
 fi
 
 rm -rf "${API_REF_DIR}"
 git clone \
-    "https://${API_REFERENCE_PUSH_TOKEN}@${GITHUB_FQDN}/${API_REF_REPO}.git" \
+    "https://${GIT_USER_NAME}@${GITHUB_FQDN}/${API_REF_REPO}.git" \
     "${API_REF_DIR}" >/dev/null 2>&1
 mkdir -p ${API_REF_DIR}/${TARGET_DIR}
 cp -f _out/apidocs/html/*.html "${API_REF_DIR}/${TARGET_DIR}/"
@@ -35,13 +35,13 @@ __EOF__
 find * -type d -regex "^v[0-9.]*" \
     -exec echo "* [{}](${GITHUB_IO_FQDN}/{}/index.html)" \; >>README.md
 
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+git config --global user.email "${GIT_AUTHOR_NAME:-kubevirt-bot}"
+git config --global user.name "${GIT_AUTHOR_EMAIL:-rmohr+kubebot@redhat.com}"
 
-# NOTE: exclude index.html from match, becasue it is static except commit hash.
+# NOTE: exclude index.html from match, because it is static except commit hash.
 if git status --porcelain | grep -v "index[.]html" | grep --quiet "^ [AM]"; then
     git add -A README.md "${TARGET_DIR}"/*.html
-    git commit --message "API Reference update by Travis Build ${TRAVIS_BUILD_NUMBER}"
+    git commit --message "API Reference update by KubeVirt Prow build ${BUILD_ID}"
 
     git push origin master >/dev/null 2>&1
     echo "API Reference updated for ${TARGET_DIR}."
