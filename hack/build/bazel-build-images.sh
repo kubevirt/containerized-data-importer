56867890c5b0f9504c07c9ec4ffbb651ee1b5ff7
Alexander Wels <awels@redhat.com>
2021-04-05 09:54:57 -0400

    Forgot to update bazel-build-images when splitting pushing. (#1733)
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- hack/build/bazel-build-images.sh
+++ hack/build/bazel-build-images.sh
@@ -31,5 +31,5 @@ for tag in ${docker_tag}; do
         --define container_prefix=${docker_prefix} \
         --define container_tag=${tag} \
         --host_force_python=PY3 \
-        //:build-images
+        //:test-container-images //cmd/cdi-operator:cdi-operator-image //cmd/cdi-controller:cdi-controller-image //cmd/cdi-apiserver:cdi-apiserver-image //cmd/cdi-cloner:cdi-cloner-image //cmd/cdi-importer:cdi-importer-image //cmd/cdi-uploadproxy:cdi-uploadproxy-image //cmd/cdi-uploadserver:cdi-uploadserver-image
 done
