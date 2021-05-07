c7265fc16647439174c98d17382d29b05d005cc2
Alexander Wels <awels@redhat.com>
2021-05-07 07:59:40 -0400

    Add extra information when CDI pods are not ready (#1762)
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- hack/build/run-functional-tests.sh
+++ hack/build/run-functional-tests.sh
@@ -78,6 +78,8 @@ done
 if [ $retry_counter -eq $MAX_CDI_WAIT_RETRY ]; then
     echo "Not all CDI pods became ready"
     ./cluster-up/kubectl.sh get pods -n $CDI_NAMESPACE
+    ./cluster-up/kubectl.sh get pods -n $CDI_NAMESPACE -o yaml
+    ./cluster-up/kubectl.sh describe pods -n $CDI_NAMESPACE
     exit 1
 fi
 
