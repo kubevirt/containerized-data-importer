c30dbfda357251e85f7ef6e98bbc215b84252886
Joel Speed <joel.speed@hotmail.co.uk>
2021-09-10 17:06:40 +0100

    Remove dead code

--- pkg/util/openapi/openapi.go
+++ pkg/util/openapi/openapi.go
@@ -2,7 +2,6 @@ package openapi
 
 import (
 	"github.com/emicklei/go-restful"
-	restfulspec "github.com/emicklei/go-restful-openapi"
 
 	"k8s.io/kube-openapi/pkg/builder"
 	"k8s.io/kube-openapi/pkg/common"
@@ -10,46 +9,6 @@ import (
 )
 
 // code stolen/adapted from https://github.com/kubevirt/kubevirt/blob/master/pkg/util/openapi/openapi.go
-
-func createOpenAPIConfig(webServices []*restful.WebService) restfulspec.Config {
-	return restfulspec.Config{
-		WebServices:    webServices,
-		WebServicesURL: "",
-		APIPath:        "/swaggerapi",
-		//PostBuildSwaggerObjectHandler: restfulspec.PostBuildSwaggerObjectFunc(addInfoToSwaggerObject),
-	}
-}
-
-func addInfoToSwaggerObject(swo *spec.Swagger) {
-	swo.Info = &spec.Info{
-		InfoProps: spec.InfoProps{
-			Title:       "KubeVirt Containerized Data Importer API",
-			Description: "Containerized Data Importer for KubeVirt.",
-			Contact: &spec.ContactInfo{
-				Name:  "kubevirt-dev",
-				Email: "kubevirt-dev@googlegroups.com",
-				URL:   "https://github.com/kubevirt/containerized-data-importer",
-			},
-			License: &spec.License{
-				Name: "Apache 2.0",
-				URL:  "https://www.apache.org/licenses/LICENSE-2.0",
-			},
-		},
-	}
-	swo.SecurityDefinitions = spec.SecurityDefinitions{
-		"BearerToken": &spec.SecurityScheme{
-			SecuritySchemeProps: spec.SecuritySchemeProps{
-				Type:        "apiKey",
-				Name:        "authorization",
-				In:          "header",
-				Description: "Bearer Token authentication",
-			},
-		},
-	}
-	swo.Swagger = "2.0"
-	swo.Security = make([]map[string][]string, 1)
-	swo.Security[0] = map[string][]string{"BearerToken": {}}
-}
 
 func createConfig(getDefinitions common.GetOpenAPIDefinitions) *common.Config {
 	return &common.Config{
