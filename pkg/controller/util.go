013cb6b62bf4de4450177ee40ddcc4d617802e76
Alexander Wels <awels@redhat.com>
2022-02-03 11:09:41 -0600

    Set http(s)_proxy to lower case env variable (#2132)
    * Set htpp(s)_proxy to lower case env variable
    
    CURL used by nbdkit doesn't read upper case http(s)_proxy environment
    variables, and thus was not using the proxy. Changed the variable to
    be lower case.
    
    Added a significant number of tests to test many more variations of
    using a proxy. Also added https + auth endpoint to the file-host
    container, so we can test https + auth with the proxy.
    
    Added https endpoint to proxy, so we can test an https proxy.
    
    Cleaned up some of the error handling in the import controller for
    the proxy, in particular if a trustedCAProxy is defined.
    
    Fixed some of the cluster wide proxy configuration so it works properly
    inside an openshift cluster.
    
    Signed-off-by: Alexander Wels <awels@redhat.com>
    
    * Add https proxy support to registry import. Added extra
    functional tests to test all registry import combinations
    
    Signed-off-by: Alexander Wels <awels@redhat.com>
    
    * Fixed some tests to work better in Open Shift.
    
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- pkg/controller/util.go
+++ pkg/controller/util.go
@@ -762,7 +762,9 @@ func GetPreallocation(client client.Client, dataVolume *cdiv1.DataVolume) bool {
 // GetClusterWideProxy returns the OpenShift cluster wide proxy object
 func GetClusterWideProxy(r client.Client) (*ocpconfigv1.Proxy, error) {
 	clusterWideProxy := &ocpconfigv1.Proxy{}
-	if err := r.Get(context.TODO(), types.NamespacedName{Name: ClusterWideProxyName}, clusterWideProxy); err != nil {
+	// Ignore both no CRD found (IgnoreIsNoMatch) and the object itself not existing IsNotFound because we want to skip if not
+	// in Open Shift.
+	if err := r.Get(context.TODO(), types.NamespacedName{Name: ClusterWideProxyName}, clusterWideProxy); IgnoreIsNoMatchError(err) != nil && !k8serrors.IsNotFound(err) {
 		return nil, err
 	}
 	return clusterWideProxy, nil
@@ -808,7 +810,7 @@ func getPriorityClass(pvc *v1.PersistentVolumeClaim) string {
 }
 
 func shouldDeletePod(pvc *v1.PersistentVolumeClaim) bool {
-	return pvc.GetAnnotations()[AnnPodRetainAfterCompletion] != "true" || pvc.DeletionTimestamp != nil
+	return pvc.GetAnnotations()[AnnPodRetainAfterCompletion] != "true" || pvc.GetAnnotations()[AnnRequiresScratch] == "true" || pvc.DeletionTimestamp != nil
 }
 
 // AddFinalizer adds a finalizer to a resource
