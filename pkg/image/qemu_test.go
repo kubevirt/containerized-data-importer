f89812c268cdfbe1edd6487e7790fc3bce1ebfbd
Bartosz Rybacki <brybacki@redhat.com>
2022-03-22 04:38:48 +0100

    Do not factor fs overhead into available space during validation (#2195)
    * Create a test for an overhead bug
    
    This image size and filesystem overhead combination was experimentally determined
    to reproduce bz#2064936 in CI when using ceph/rbd with a Filesystem mode PV since
    the filesystem capacity will be constrained by the PVC request size.
    
    Below is the problem it tries to recreate:
    When validating whether an image will fit into a PV we compare the
    image's virtual size to the filesystem's reported available space to
    guage whether it will fit.  The current calculation reduces the apparent
    available space by the configured filesystem overhead value but the
    overhead is already (mostly) factored into the result of Statfs.  This
    causes the check to fail for PVCs that are just large enough to
    accommodate an image plus overhead (ie. when using the DataVolume
    Storage API with filesystem PVs with capacity constrained by the PVC
    storage request size).
    
    This was not caught in testing because HPP does not have capacity
    constrained PVs and we are typically testing block volumes in the ceph
    lanes.  It can be triggered in our CI by allocating a Filesystem PV on
    ceph-rbd storage because these volumes are capacity constrained and
    subject to filesystem overhead.
    
    Signed-off-by: Bartosz Rybacki <brybacki@redhat.com>
    
    * Fix a target pvc validation bug
    
    Corrects the validation logic for target volume.
    
    Below description of the original problem:
    When validating whether an image will fit into a PV we compare the
    image's virtual size to the filesystem's reported available space to
    guage whether it will fit.  The current calculation reduces the apparent
    available space by the configured filesystem overhead value but the
    overhead is already (mostly) factored into the result of Statfs.  This
    causes the check to fail for PVCs that are just large enough to
    accommodate an image plus overhead (ie. when using the DataVolume
    Storage API with filesystem PVs with capacity constrained by the PVC
    storage request size).
    
    This was not caught in testing because HPP does not have capacity
    constrained PVs and we are typically testing block volumes in the ceph
    lanes.  It can be triggered in our CI by allocating a Filesystem PV on
    ceph-rbd storage because these volumes are capacity constrained and
    subject to filesystem overhead.
    
    Signed-off-by: Bartosz Rybacki <brybacki@redhat.com>
    
    * Improve the warning message
    
    Removed redundant and misleading part about pvc size and update the simplification
    
    Signed-off-by: Bartosz Rybacki <brybacki@redhat.com>
    
    * Remove useless test
    
    The test checks that the validation logic takes fs Overhead into account.
    New validation logic does not check fs overhead. So test is no longer
    relevant.
    
    Signed-off-by: Bartosz Rybacki <brybacki@redhat.com>

--- pkg/image/qemu_test.go
+++ pkg/image/qemu_test.go
@@ -217,9 +217,9 @@ var _ = Describe("Resize", func() {
 var _ = Describe("Validate", func() {
 	imageName, _ := url.Parse("myimage.qcow2")
 
-	table.DescribeTable("Validate should", func(execfunc execFunctionType, errString string, image *url.URL, overhead float64) {
+	table.DescribeTable("Validate should", func(execfunc execFunctionType, errString string, image *url.URL) {
 		replaceExecFunction(execfunc, func() {
-			err := Validate(image, 42949672960, overhead)
+			err := Validate(image, 42949672960)
 
 			if errString == "" {
 				Expect(err).NotTo(HaveOccurred())
@@ -232,13 +232,12 @@ var _ = Describe("Validate", func() {
 			}
 		})
 	},
-		table.Entry("should return success", mockExecFunction(goodValidateJSON, "", expectedLimits, "info", "--output=json", imageName.String()), "", imageName, 0.0),
-		table.Entry("should return error", mockExecFunction("explosion", "exit 1", expectedLimits), "explosion, exit 1", imageName, 0.0),
-		table.Entry("should return error on bad json", mockExecFunction(badValidateJSON, "", expectedLimits), "unexpected end of JSON input", imageName, 0.0),
-		table.Entry("should return error on bad format", mockExecFunction(badFormatValidateJSON, "", expectedLimits), fmt.Sprintf("Invalid format raw2 for image %s", imageName), imageName, 0.0),
-		table.Entry("should return error on invalid backing file", mockExecFunction(backingFileValidateJSON, "", expectedLimits), fmt.Sprintf("Image %s is invalid because it has invalid backing file backing-file.qcow2", imageName), imageName, 0.0),
-		table.Entry("should return error when PVC is too small", mockExecFunction(hugeValidateJSON, "", expectedLimits), fmt.Sprintf("Virtual image size %d is larger than available size %d (PVC size %d, reserved overhead %f%%). A larger PVC is required.", 52949672960, 42949672960, 52949672960, 0.0), imageName, 0.0),
-		table.Entry("should return error when PVC is too small with overhead", mockExecFunction(hugeValidateJSON, "", expectedLimits), fmt.Sprintf("Virtual image size %d is larger than available size %d (PVC size %d, reserved overhead %f%%). A larger PVC is required.", 52949672960, 34359738368, 52949672960, 0.2), imageName, 0.2),
+		table.Entry("should return success", mockExecFunction(goodValidateJSON, "", expectedLimits, "info", "--output=json", imageName.String()), "", imageName),
+		table.Entry("should return error", mockExecFunction("explosion", "exit 1", expectedLimits), "explosion, exit 1", imageName),
+		table.Entry("should return error on bad json", mockExecFunction(badValidateJSON, "", expectedLimits), "unexpected end of JSON input", imageName),
+		table.Entry("should return error on bad format", mockExecFunction(badFormatValidateJSON, "", expectedLimits), fmt.Sprintf("Invalid format raw2 for image %s", imageName), imageName),
+		table.Entry("should return error on invalid backing file", mockExecFunction(backingFileValidateJSON, "", expectedLimits), fmt.Sprintf("Image %s is invalid because it has invalid backing file backing-file.qcow2", imageName), imageName),
+		table.Entry("should return error when PVC is too small", mockExecFunction(hugeValidateJSON, "", expectedLimits), fmt.Sprintf("Virtual image size %d is larger than the reported available storage %d. A larger PVC is required.", 52949672960, 42949672960), imageName),
 	)
 
 })
