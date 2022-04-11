93e4fc59ce6696e3f75c98ded48e60f142049d4d
Alexander Wels <awels@redhat.com>
2022-04-11 17:42:04 -0500

    Add lane scripts in repo (#2218)
    Instead of setting various variables in project-infra set them
    in scripts inside this repo instead. This allows us to upgrade
    k8s versions without having to modify the project infra lanes
    
    Signed-off-by: Alexander Wels <awels@redhat.com>

---
+++ automation/destructive.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# This file is part of the KubeVirt project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright 2022 Red Hat, Inc.
+#
+
+set -ex
+export TARGET=k8s-1.22
+export KUBEVIRT_STORAGE=hpp
+export KUBEVIRT_DEPLOY_PROMETHEUS=true
+export CDI_E2E_FOCUS=Destructive
+automation/test.sh
