121d4655f2caa8708ba207fc62ff94630b4318d4
Arnon Gilboa <agilboa@redhat.com>
2022-05-25 09:52:05 +0300

    Get rid of unnecessary DIC reconcile updates
    Also fixed status.lastExecutionTimestamp to be the last polling time
    as intended in the design, and not the last reconcile update time.
    
    Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

--- pkg/controller/dataimportcron-controller.go
+++ pkg/controller/dataimportcron-controller.go
@@ -247,9 +247,7 @@ func (r *DataImportCronReconciler) update(ctx context.Context, dataImportCron *c
 	log := r.log.WithName("update")
 	res := reconcile.Result{}
 
-	now := metav1.Now()
-	dataImportCron.Status.LastExecutionTimestamp = &now
-
+	dataImportCronCopy := dataImportCron.DeepCopy()
 	importSucceeded := false
 	imports := dataImportCron.Status.CurrentImports
 	dataVolume := &cdiv1.DataVolume{}
@@ -324,8 +322,10 @@ func (r *DataImportCronReconciler) update(ctx context.Context, dataImportCron *c
 		updateDataImportCronCondition(dataImportCron, cdiv1.DataImportCronUpToDate, corev1.ConditionFalse, "No source digest", noDigest)
 	}
 
-	if err := r.client.Update(ctx, dataImportCron); err != nil {
-		return res, err
+	if !reflect.DeepEqual(dataImportCron, dataImportCronCopy) {
+		if err := r.client.Update(ctx, dataImportCron); err != nil {
+			return res, err
+		}
 	}
 	return res, nil
 }
@@ -358,6 +358,7 @@ func (r *DataImportCronReconciler) updateImageStreamDesiredDigest(ctx context.Co
 	if regSource.ImageStream == nil {
 		return nil
 	}
+	now := metav1.Now()
 	imageStream, imageStreamTag, err := r.getImageStream(ctx, *regSource.ImageStream, dataImportCron.Namespace)
 	if err != nil {
 		return err
@@ -366,6 +367,7 @@ func (r *DataImportCronReconciler) updateImageStreamDesiredDigest(ctx context.Co
 	if err != nil {
 		return err
 	}
+	dataImportCron.Status.LastExecutionTimestamp = &now
 	if digest != "" && dataImportCron.Annotations[AnnSourceDesiredDigest] != digest {
 		log.Info("Updating DataImportCron", "digest", digest)
 		AddAnnotation(dataImportCron, AnnSourceDesiredDigest, digest)
