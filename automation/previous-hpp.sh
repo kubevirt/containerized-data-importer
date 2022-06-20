33ec39d924b5e9ef145bdf6e4788018f697c3c82
Alex Kalenyuk <akalenyu@redhat.com>
2022-06-20 15:46:48 +0300

    Increment k8s versions used in CI lanes
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- automation/previous-hpp.sh
+++ automation/previous-hpp.sh
@@ -18,7 +18,7 @@
 #
 
 set -ex
-export TARGET=k8s-1.22
+export TARGET=k8s-1.23
 export KUBEVIRT_STORAGE=hpp
 export KUBEVIRT_DEPLOY_PROMETHEUS=true
 export CDI_E2E_SKIP=Destructive
--- automation/upgrade.sh
+++ automation/upgrade.sh
@@ -18,7 +18,7 @@
 #
 
 set -ex
-export TARGET=k8s-1.23
+export TARGET=k8s-1.24
 export KUBEVIRT_STORAGE=hpp
 export MULTI_UPGRADE=true
 export CDI_E2E_SKIP=Destructive
