dcf968e1c5cd18e412ad8491decafd5490296512
Michael Henriksen <mhenriks@redhat.com>
2022-06-28 08:19:11 -0400

    fix issue #2323: Cross namespace smart clone not cleaning up (#2333)
    * fix issue #2323: Cross namespace smart clone leaving tmp PVC and ObjectTransfer around
    
    Signed-off-by: Michael Henriksen <mhenriks@redhat.com>
    
    * wait for objecttransfers to be cleaned up in clone tests
    
    Signed-off-by: Michael Henriksen <mhenriks@redhat.com>
    
    * attempt to address test failures
    
    Signed-off-by: Michael Henriksen <mhenriks@redhat.com>
    
    * move cross namespace cleanup code
    
    Signed-off-by: Michael Henriksen <mhenriks@redhat.com>
    
    * move cleanup transfer call until after dataSource is resolved
    
    Signed-off-by: Michael Henriksen <mhenriks@redhat.com>

--- pkg/controller/datavolume-controller.go
+++ pkg/controller/datavolume-controller.go
@@ -545,6 +545,12 @@ func (r *DatavolumeReconciler) Reconcile(_ context.Context, req reconcile.Reques
 		return reconcile.Result{}, err
 	}
 
+	if isCrossNamespaceClone(datavolume) && datavolume.Status.Phase == cdiv1.Succeeded {
+		if err := r.cleanupTransfer(log, datavolume, transferName); err != nil {
+			return reconcile.Result{}, err
+		}
+	}
+
 	pvcPopulated := false
 	// Get the pvc with the name specified in DataVolume.spec
 	pvc := &corev1.PersistentVolumeClaim{}
@@ -682,7 +688,7 @@ func (r *DatavolumeReconciler) reconcileClone(log logr.Logger,
 		}
 	}
 
-	if !prePopulated && !pvcPopulated {
+	if !prePopulated {
 		if pvc == nil {
 			if selectedCloneStrategy == SmartClone {
 				snapshotClassName, _ := r.getSnapshotClassForSmartClone(datavolume, pvcSpec)
@@ -724,8 +730,10 @@ func (r *DatavolumeReconciler) reconcileClone(log logr.Logger,
 
 		switch selectedCloneStrategy {
 		case HostAssistedClone:
-			if err := r.ensureExtendedToken(pvc); err != nil {
-				return reconcile.Result{}, err
+			if !pvcPopulated {
+				if err := r.ensureExtendedToken(pvc); err != nil {
+					return reconcile.Result{}, err
+				}
 			}
 		case CsiClone:
 			switch pvc.Status.Phase {
@@ -746,7 +754,9 @@ func (r *DatavolumeReconciler) reconcileClone(log logr.Logger,
 			}
 			fallthrough
 		case SmartClone:
-			return r.finishClone(log, datavolume, pvc, pvcSpec, transferName, selectedCloneStrategy)
+			if !pvcPopulated {
+				return r.finishClone(log, datavolume, pvc, pvcSpec, transferName, selectedCloneStrategy)
+			}
 		}
 	}
 
@@ -947,15 +957,6 @@ func (r *DatavolumeReconciler) finishClone(log logr.Logger,
 	pvcSpec *corev1.PersistentVolumeClaimSpec,
 	transferName string,
 	selectedCloneStrategy cloneStrategy) (reconcile.Result, error) {
-
-	if isCrossNamespaceClone(datavolume) && datavolume.Status.Phase == cdiv1.Succeeded {
-		if err := r.cleanupTransfer(log, datavolume, transferName); err != nil {
-			return reconcile.Result{}, err
-		}
-
-		// done, done
-		return reconcile.Result{}, nil
-	}
 
 	//DO Nothing, not yet ready
 	if pvc.Annotations[AnnCloneOf] != "true" {
@@ -1420,7 +1421,7 @@ func (r *DatavolumeReconciler) cleanupTransfer(log logr.Logger, dv *cdiv1.DataVo
 		return nil
 	}
 
-	log.Info("Doing cleanup")
+	log.V(1).Info("Doing cleanup")
 
 	if dv.DeletionTimestamp != nil && dv.Status.Phase != cdiv1.Succeeded {
 		// delete all potential PVCs that may not have owner refs
