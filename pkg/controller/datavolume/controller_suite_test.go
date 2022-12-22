04fdeb72848341ea53c32260085a2a1ecb96e290
Alex Kalenyuk <akalenyu@redhat.com>
2022-12-22 16:11:30 +0200

    Create unit test suite for new folders following refactor
    Otherwise we skip the tests in these
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

---
+++ pkg/controller/datavolume/controller_suite_test.go
@@ -0,0 +1,22 @@
+package datavolume_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"kubevirt.io/containerized-data-importer/tests/reporters"
+)
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+})
+
+func TestController(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecsWithDefaultAndCustomReporters(t, "Datavolume controllers Suite", reporters.NewReporters())
+}
