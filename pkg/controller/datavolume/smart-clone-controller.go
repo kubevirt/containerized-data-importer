92382bdffe14b8c106e16724003d1457f84d2d7c
Alexander Wels <awels@redhat.com>
2023-03-31 11:52:55 -0500

    Allow restoreSize to be 0, and use request size if 0
    Some CSI provisioner when making volume snapshots don't set the
    restore size because it is hard to figure out the value or the
    actual stored size on the storage is 0 (Copy on Write). There was
    a strict check in place in case of CSI drivers not allowing a
    restore to a size other than the restoreSize of the snapshot.
    
    This makes the check more lenient that when we encounter a 0 size
    restoreSize, we can assume we can use the requested size value in
    the pvc
    
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- pkg/controller/datavolume/smart-clone-controller.go
+++ pkg/controller/datavolume/smart-clone-controller.go
@@ -356,8 +356,21 @@ func (r *SmartCloneReconciler) getTargetPVC(dataVolume *cdiv1.DataVolume) (*core
 func newPvcFromSnapshot(dv *cdiv1.DataVolume, name string, snapshot *snapshotv1.VolumeSnapshot, targetPvcSpec *corev1.PersistentVolumeClaimSpec) (*corev1.PersistentVolumeClaim, error) {
 	targetPvcSpecCopy := targetPvcSpec.DeepCopy()
 	restoreSize := snapshot.Status.RestoreSize
-	if restoreSize == nil {
+	if restoreSize == nil || restoreSize.Sign() == -1 {
 		return nil, fmt.Errorf("snapshot has no RestoreSize")
+	}
+	if restoreSize.IsZero() {
+		// Don't error out if the restoreSize is 0. Use the size from the DataVolume instead
+		if dv.Spec.PVC != nil {
+			reqSize := dv.Spec.PVC.Resources.Requests[corev1.ResourceStorage]
+			restoreSize = &reqSize
+		} else if dv.Spec.Storage != nil {
+			reqSize := dv.Spec.Storage.Resources.Requests[corev1.ResourceStorage]
+			restoreSize = &reqSize
+		}
+		if restoreSize.IsZero() {
+			return nil, fmt.Errorf("unable to determine restore size of PVC")
+		}
 	}
 
 	key, err := cache.MetaNamespaceKeyFunc(snapshot)
