kubevirt/containerized-data-importer

commit fe01d99eb7a01f68ed8aa36edbc18b9919714179

[release-v1.55] Update succeeded DIC PVC labels also when no DV GC (#2540)
Manual backport of #2539

Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

pkg/controller/dataimportcron-controller.go
@@ -311,6 +311,9 @@ func (r *DataImportCronReconciler) update(ctx context.Context, dataImportCron *c
 	if dv != nil {
 		switch dv.Status.Phase {
 		case cdiv1.Succeeded:
+			if r.updatePvc(ctx, dataImportCron, pvc); err != nil {
+				return res, err
+			}
 			importSucceeded = true
 		case cdiv1.ImportScheduled:
 			updateDataImportCronCondition(dataImportCron, cdiv1.DataImportCronProgressing, corev1.ConditionFalse, "Import is scheduled", scheduled)
@@ -321,14 +324,10 @@ func (r *DataImportCronReconciler) update(ctx context.Context, dataImportCron *c
 			updateDataImportCronCondition(dataImportCron, cdiv1.DataImportCronProgressing, corev1.ConditionFalse, fmt.Sprintf("Import DataVolume phase %s", dvPhase), dvPhase)
 		}
 	} else if pvc != nil {
-		importSucceeded = true
-		pvcCopy := pvc.DeepCopy()
-		r.setDataImportCronResourceLabels(dataImportCron, pvc)
-		if !reflect.DeepEqual(pvc, pvcCopy) {
-			if err := r.client.Update(ctx, pvc); err != nil {
-				return res, err
-			}
+		if r.updatePvc(ctx, dataImportCron, pvc); err != nil {
+			return res, err
 		}
+		importSucceeded = true
 	} else {
 		if len(imports) > 0 {
 			dataImportCron.Status.CurrentImports = imports[1:]
@@ -392,7 +391,7 @@ func (r *DataImportCronReconciler) update(ctx context.Context, dataImportCron *c
 	return res, nil
 }
 
-// Returns the current import DV if exists, otherwise returns the last imported PVC
+// Returns the current import DV if exists, and the last imported PVC
 func (r *DataImportCronReconciler) getImportState(ctx context.Context, cron *cdiv1.DataImportCron) (*cdiv1.DataVolume, *corev1.PersistentVolumeClaim, error) {
 	imports := cron.Status.CurrentImports
 	if len(imports) == 0 {
@@ -401,19 +400,32 @@ func (r *DataImportCronReconciler) getImportState(ctx context.Context, cron *cdi
 
 	dvName := imports[0].DataVolumeName
 	dv := &cdiv1.DataVolume{}
-	if err := r.client.Get(ctx, types.NamespacedName{Namespace: cron.Namespace, Name: dvName}, dv); err == nil {
-		return dv, nil, nil
-	} else if !k8serrors.IsNotFound(err) {
-		return nil, nil, err
+	if err := r.client.Get(ctx, types.NamespacedName{Namespace: cron.Namespace, Name: dvName}, dv); err != nil {
+		if !k8serrors.IsNotFound(err) {
+			return nil, nil, err
+		}
+		dv = nil
 	}
 
 	pvc := &corev1.PersistentVolumeClaim{}
-	if err := r.client.Get(ctx, types.NamespacedName{Namespace: cron.Namespace, Name: dvName}, pvc); err == nil {
-		return nil, pvc, nil
-	} else if !k8serrors.IsNotFound(err) {
-		return nil, nil, err
+	if err := r.client.Get(ctx, types.NamespacedName{Namespace: cron.Namespace, Name: dvName}, pvc); err != nil {
+		if !k8serrors.IsNotFound(err) {
+			return nil, nil, err
+		}
+		pvc = nil
 	}
-	return nil, nil, nil
+	return dv, pvc, nil
+}
+
+func (r *DataImportCronReconciler) updatePvc(ctx context.Context, cron *cdiv1.DataImportCron, pvc *corev1.PersistentVolumeClaim) error {
+	pvcCopy := pvc.DeepCopy()
+	r.setDataImportCronResourceLabels(cron, pvc)
+	if !reflect.DeepEqual(pvc, pvcCopy) {
+		if err := r.client.Update(ctx, pvc); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (r *DataImportCronReconciler) deleteErroneousDataVolume(ctx context.Context, cron *cdiv1.DataImportCron, dv *cdiv1.DataVolume) error {

pkg/controller/dataimportcron-controller_test.go
@@ -349,6 +349,11 @@ var _ = Describe("All DataImportCron Tests", func() {
 			dv.Status.Phase = cdiv1.Succeeded
 			err = reconciler.client.Update(context.TODO(), dv)
 			Expect(err).ToNot(HaveOccurred())
+
+			pvc := createPvc(dv.Name, dv.Namespace, nil, nil)
+			err = reconciler.client.Create(context.TODO(), pvc)
+			Expect(err).ToNot(HaveOccurred())
+
 			verifyConditions("Import succeeded", false, true, true, noImport, upToDate, ready)
 
 			sourcePVC := cdiv1.DataVolumeSourcePVC{
@@ -569,6 +574,10 @@ var _ = Describe("All DataImportCron Tests", func() {
 			dv.Status.Phase = cdiv1.Succeeded
 			dv.Status.Conditions = updateReadyCondition(dv.Status.Conditions, corev1.ConditionTrue, "", "")
 			err = reconciler.client.Update(context.TODO(), dv)
+			Expect(err).ToNot(HaveOccurred())
+
+			pvc := createPvc(dv.Name, dv.Namespace, nil, nil)
+			err = reconciler.client.Create(context.TODO(), pvc)
 			Expect(err).ToNot(HaveOccurred())
 			verifyConditions("Import succeeded", false, true, true, noImport, upToDate, ready)
 

tests/dataimportcron_test.go
@@ -125,9 +125,8 @@ var _ = Describe("DataImportCron", func() {
 					By("Reset desired digest")
 					retryOnceOnErr(updateDataImportCron(f.CdiClient, ns, cronName, updateDigest(""))).Should(BeNil())
 
-					By("Delete last import PVC")
-					err := f.K8sClient.CoreV1().PersistentVolumeClaims(ns).Delete(context.TODO(), currentImportDv, metav1.DeleteOptions{})
-					Expect(err).ToNot(HaveOccurred())
+					By("Delete last import PVC " + currentImportDv)
+					deleteDvPvc(f, currentImportDv)
 					lastImportDv = ""
 
 					By("Wait for non-empty desired digest")
@@ -198,8 +197,7 @@ var _ = Describe("DataImportCron", func() {
 			}, dataImportCronTimeout, pollingInterval).Should(BeTrue(), "DataSource was not re-created")
 
 			By("Delete last imported PVC")
-			err = f.DeletePVC(currentPvc)
-			Expect(err).To(BeNil())
+			deleteDvPvc(f, currentPvc.Name)
 			By("Verify last imported PVC was re-created")
 			Eventually(func() bool {
 				pvc, err := f.K8sClient.CoreV1().PersistentVolumeClaims(ns).Get(context.TODO(), currentPvc.Name, metav1.GetOptions{})

tests/datasource_test.go
@@ -64,14 +64,6 @@ var _ = Describe("DataSource", func() {
 		f.ForceBindIfWaitForFirstConsumer(pvc)
 	}
 
-	// Delete PVC if DV was GCed, otherwise delete both
-	deleteDvPvc := func(pvcName string) {
-		err := utils.DeleteDataVolume(f.CdiClient, f.Namespace.Name, pvcName)
-		Expect(err).ToNot(HaveOccurred())
-		err = utils.DeletePVC(f.K8sClient, f.Namespace.Name, pvcName)
-		Expect(err).ToNot(HaveOccurred())
-	}
-
 	It("[test_id:8041]status conditions should be updated on pvc create/update/delete", func() {
 		By("creating datasource")
 		ds := newDataSource(ds1Name)
@@ -87,7 +79,7 @@ var _ = Describe("DataSource", func() {
 		createDv(pvc1Name, testUrl())
 		ds = waitForReadyCondition(ds, corev1.ConditionTrue, "Ready")
 
-		deleteDvPvc(pvc1Name)
+		deleteDvPvc(f, pvc1Name)
 		ds = waitForReadyCondition(ds, corev1.ConditionFalse, "NotFound")
 
 		ds.Spec.Source.PVC = nil
@@ -120,7 +112,7 @@ var _ = Describe("DataSource", func() {
 		ds1 = waitForReadyCondition(ds1, corev1.ConditionTrue, "Ready")
 		ds2 = waitForReadyCondition(ds2, corev1.ConditionTrue, "Ready")
 
-		deleteDvPvc(pvc1Name)
+		deleteDvPvc(f, pvc1Name)
 		ds1 = waitForReadyCondition(ds1, corev1.ConditionFalse, "NotFound")
 		ds2 = waitForReadyCondition(ds2, corev1.ConditionFalse, "NotFound")
 
@@ -130,7 +122,7 @@ var _ = Describe("DataSource", func() {
 		ds1 = waitForReadyCondition(ds1, corev1.ConditionFalse, "ImportInProgress")
 		ds2 = waitForReadyCondition(ds2, corev1.ConditionFalse, "ImportInProgress")
 
-		deleteDvPvc(pvc2Name)
+		deleteDvPvc(f, pvc2Name)
 		ds1 = waitForReadyCondition(ds1, corev1.ConditionFalse, "NotFound")
 		ds2 = waitForReadyCondition(ds2, corev1.ConditionFalse, "NotFound")
 	})
@@ -158,3 +150,11 @@ var _ = Describe("DataSource", func() {
 		}, 60*time.Second, pollingInterval).ShouldNot(Equal(ts))
 	})
 })
+
+// Delete PVC if DV was GCed, otherwise delete both
+func deleteDvPvc(f *framework.Framework, pvcName string) {
+	err := utils.DeleteDataVolume(f.CdiClient, f.Namespace.Name, pvcName)
+	Expect(err).ToNot(HaveOccurred())
+	err = utils.DeletePVC(f.K8sClient, f.Namespace.Name, pvcName)
+	Expect(err).ToNot(HaveOccurred())
+}

commit b1e61b306b2336bcdf977e45ef919712c348a9a6

[release-v1.55] Fix DataImportCron PVC update error check
Fixed in #2696 in main/release-v1.57 as part of the linter error fixes.

PVC lastUseTime annotation was sometimes not updated so DataImportCron
PVC garbage collection was misbehaving and deleting the latest imported
PVC, which was then re-imported again until the update succeeded.

The issue was introduced by #2540 and found due to flaky [test_id:7406].

bz #2222011

Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

pkg/controller/dataimportcron-controller.go
@@ -307,7 +307,7 @@ func (r *DataImportCronReconciler) update(ctx context.Context, dataImportCron *c
 	if dv != nil {
 		switch dv.Status.Phase {
 		case cdiv1.Succeeded:
-			if r.updatePvc(ctx, dataImportCron, pvc); err != nil {
+			if err = r.updatePvc(ctx, dataImportCron, pvc); err != nil {
 				return res, err
 			}
 			importSucceeded = true
@@ -320,7 +320,7 @@ func (r *DataImportCronReconciler) update(ctx context.Context, dataImportCron *c
 			updateDataImportCronCondition(dataImportCron, cdiv1.DataImportCronProgressing, corev1.ConditionFalse, fmt.Sprintf("Import DataVolume phase %s", dvPhase), dvPhase)
 		}
 	} else if pvc != nil {
-		if r.updatePvc(ctx, dataImportCron, pvc); err != nil {
+		if err = r.updatePvc(ctx, dataImportCron, pvc); err != nil {
 			return res, err
 		}
 		importSucceeded = true