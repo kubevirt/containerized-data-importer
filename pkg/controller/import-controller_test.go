5c561fb15d8e755616c456289fbd390ff2b17da0
Alex Kalenyuk <akalenyu@redhat.com>
2023-08-13 16:35:41 +0300

    Don't use scratch space for registry node pull imports
    When importing via node container runtime cache, we always have the image handy locally.
    This manifests itself in the form of a bug where we loop over
    ```bash
    E0813 13:32:38.443088       1 data-processor.go:251] scratch space required and none found
    E0813 13:32:38.443102       1 importer.go:181] scratch space required and none found
    ```
    On registry node pull imports where images are not raw
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- pkg/controller/import-controller_test.go
+++ pkg/controller/import-controller_test.go
@@ -1049,6 +1049,10 @@ func createImportTestEnv(podEnvVar *importPodEnvVar, uid string) []corev1.EnvVar
 			Value: podEnvVar.uuid,
 		},
 		{
+			Name:  common.ImporterPullMethod,
+			Value: podEnvVar.pullMethod,
+		},
+		{
 			Name:  common.ImporterReadyFile,
 			Value: podEnvVar.readyFile,
 		},
