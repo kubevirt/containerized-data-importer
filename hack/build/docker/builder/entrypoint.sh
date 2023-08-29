a58dfc28b07ad3433b2778e987c7b80a7ab27aaa
Alexander Wels <awels@redhat.com>
2023-08-29 13:28:40 -0500

    Trigger builder release
    Signed-off-by: Alexander Wels <awels@redhat.com>

--- hack/build/docker/builder/entrypoint.sh
+++ hack/build/docker/builder/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#Copyright 2018 The CDI Authors.
+#Copyright 2023 The CDI Authors.
 #
 #Licensed under the Apache License, Version 2.0 (the "License");
 #you may not use this file except in compliance with the License.
