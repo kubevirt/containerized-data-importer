4fe9f7d607871aa3438c02a8cf43f0028246bc77
Alvaro Romero <alromero@redhat.com>
2023-11-02 10:44:49 +0100

    Test import with unexpected content-type
    This commit adds a test to cover a import with unexpected content-type. It also updates the badserver to allow mocking this behavior.
    
    Signed-off-by: Alvaro Romero <alromero@redhat.com>

--- tests/badserver_test.go
+++ tests/badserver_test.go
@@ -2,9 +2,14 @@ package tests
 
 import (
 	"fmt"
+	"strings"
+	"time"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	"kubevirt.io/containerized-data-importer/pkg/common"
+	controller "kubevirt.io/containerized-data-importer/pkg/controller/common"
 	"kubevirt.io/containerized-data-importer/tests/framework"
 	"kubevirt.io/containerized-data-importer/tests/utils"
 
@@ -30,6 +35,31 @@ var _ = Describe("Problematic server responses", func() {
 		Entry("[rfe_id:4191][test_id:4193][crit:low][vendor:cnv-qe@redhat.com][level:component] Should succeed even on a flaky server", "/flaky/cirros-qcow2.img"),
 		Entry("[rfe_id:4326][test_id:5076][crit:low][vendor:cnv-qe@redhat.com][level:component] Should succeed even if Accept-Ranges doesn't exist", "/no-accept-ranges/cirros-qcow2.img"),
 	)
+
+	It("Should warn against unexpected content-types when importing a kubevirt img", func() {
+		cdiBadServer := fmt.Sprintf("http://cdi-bad-webserver.%s:9090", f.CdiInstallNs)
+		pathname := "/bad-content-type/cirros-qcow2.img"
+		dataVolume = utils.NewDataVolumeWithHTTPImport("badserver-dv", "1Gi", cdiBadServer+pathname)
+		dataVolume.Annotations[controller.AnnPodRetainAfterCompletion] = "true"
+		By("creating DataVolume")
+		dataVolume, err := utils.CreateDataVolumeFromDefinition(f.CdiClient, f.Namespace.Name, dataVolume)
+		Expect(err).ToNot(HaveOccurred())
+		f.ForceBindPvcIfDvIsWaitForFirstConsumer(dataVolume)
+
+		err = utils.WaitForDataVolumePhase(f, f.Namespace.Name, cdiv1.Succeeded, dataVolume.Name)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("Find importer pod after completion")
+		importer, err := utils.FindPodByPrefixOnce(f.K8sClient, dataVolume.Namespace, common.ImporterPodName, common.CDILabelSelector)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(importer.DeletionTimestamp).To(BeNil())
+
+		By("Verify HTTP request error in importer log")
+		Eventually(func() bool {
+			log, _ := f.RunKubectlCommand("logs", importer.Name, "-n", importer.Namespace)
+			return strings.Contains(log, "Unexpected content type 'text/html'. Content might not be a KubeVirt image.")
+		}, time.Minute, pollingInterval).Should(BeTrue())
+	})
 
 	AfterEach(func() {
 		By("deleting DataVolume")
