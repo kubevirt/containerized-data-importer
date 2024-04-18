e2ad069849d245139d6e0233d28e9267ee1ebcbb
Edu Gómez Escandell <egomez@redhat.com>
2024-04-18 11:02:33 +0200

    Run 'golangci-lint --fix' when using 'make format'
    Signed-off-by: Edu Gómez Escandell <egomez@redhat.com>

--- hack/build/run-linters.sh
+++ hack/build/run-linters.sh
@@ -4,4 +4,5 @@ GOLANGCI_VERSION="${GOLANGCI_VERSION:-v1.54.2}"
 
 go install "github.com/golangci/golangci-lint/cmd/golangci-lint@${GOLANGCI_VERSION}"
 
-golangci-lint run --timeout=16m ./...
+golangci-lint run --timeout=16m ./... "$@"
+
