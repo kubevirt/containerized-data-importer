b8a617ecd8c5faa295edbdb0afca811d5a60a94f
Alex Kalenyuk <akalenyu@redhat.com>
2024-04-21 17:00:26 +0300

    Correct wrong check of resulting image sparsiness
    The first issue is that we look at content size since 2168,
    which is not beneficial in the sparseness check as opposed to disk usage.
    
    The second issue is that the check we have in tests for sparsiness is not honest because of the "equal to" part.
    The virtual size has to be strictly greater than the content
    (as the content is displayed by tools that understand sparseness).
    
    The third issue is that we almost always resize which results in significantly larger virtual size.
    What we really care about is comparing against the original virtual size of the image.
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- tests/upload_test.go
+++ tests/upload_test.go
@@ -199,7 +199,7 @@ var _ = Describe("[rfe_id:138][crit:high][vendor:cnv-qe@redhat.com][level:compon
 				Expect(err).ToNot(HaveOccurred())
 				Expect(same).To(BeTrue())
 				By("Verifying the image is sparse")
-				Expect(f.VerifySparse(f.Namespace, pvc, utils.DefaultImagePath)).To(BeTrue())
+				Expect(f.VerifySparse(f.Namespace, pvc, utils.DefaultImagePath, utils.UploadFileSize)).To(BeTrue())
 				if utils.DefaultStorageCSIRespectsFsGroup {
 					// CSI storage class, it should respect fsGroup
 					By("Checking that disk image group is qemu")
@@ -414,7 +414,7 @@ var _ = Describe("[rfe_id:138][crit:high][vendor:cnv-qe@redhat.com][level:compon
 					Expect(err).ToNot(HaveOccurred())
 					Expect(same).To(BeTrue())
 					By("Verifying the image is sparse")
-					Expect(f.VerifySparse(f.Namespace, archivePVC, pathInPvc)).To(BeTrue())
+					Expect(f.VerifySparse(f.Namespace, archivePVC, pathInPvc, utils.UploadFileSize)).To(BeTrue())
 				}
 			} else {
 				checkFailureNoValidToken(archivePVC)
@@ -529,7 +529,7 @@ var _ = Describe("[rfe_id:138][crit:high][vendor:cnv-qe@redhat.com][level:compon
 						Expect(err).ToNot(HaveOccurred())
 						Expect(same).To(BeTrue())
 						By("Verifying the image is sparse")
-						Expect(f.VerifySparse(f.Namespace, pvc, utils.DefaultImagePath)).To(BeTrue())
+						Expect(f.VerifySparse(f.Namespace, pvc, utils.DefaultImagePath, utils.UploadFileSize)).To(BeTrue())
 						if utils.DefaultStorageCSIRespectsFsGroup {
 							// CSI storage class, it should respect fsGroup
 							By("Checking that disk image group is qemu")
@@ -603,7 +603,7 @@ var _ = Describe("[rfe_id:138][crit:high][vendor:cnv-qe@redhat.com][level:compon
 				Expect(err).ToNot(HaveOccurred())
 				Expect(same).To(BeTrue())
 				By("Verifying the image is sparse")
-				Expect(f.VerifySparse(f.Namespace, pvc, utils.DefaultImagePath)).To(BeTrue())
+				Expect(f.VerifySparse(f.Namespace, pvc, utils.DefaultImagePath, utils.UploadFileSize)).To(BeTrue())
 				if utils.DefaultStorageCSIRespectsFsGroup {
 					// CSI storage class, it should respect fsGroup
 					By("Checking that disk image group is qemu")
@@ -734,7 +734,7 @@ var _ = Describe("[rfe_id:138][crit:high][vendor:cnv-qe@redhat.com][level:compon
 						Expect(err).ToNot(HaveOccurred())
 						Expect(same).To(BeTrue())
 						By("Verifying the image is sparse")
-						Expect(f.VerifySparse(f.Namespace, pvc, pathInPvc)).To(BeTrue())
+						Expect(f.VerifySparse(f.Namespace, pvc, pathInPvc, utils.UploadFileSize)).To(BeTrue())
 					}
 				} else {
 					checkFailureNoValidToken(pvcPrime)
