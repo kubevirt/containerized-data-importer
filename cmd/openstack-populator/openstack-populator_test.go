780c0915fd33126e9c2e6e34a102c83f6b10ef51
Benny Zlotnik <bzlotnik@redhat.com>
2024-04-28 11:35:15 +0300

    forklift: use ginkgo for openstack populator test
    Signed-off-by: Benny Zlotnik <bzlotnik@redhat.com>

--- cmd/openstack-populator/openstack-populator_test.go
+++ cmd/openstack-populator/openstack-populator_test.go
@@ -7,7 +7,9 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"testing"
+
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
 )
 
 func setupMockServer() (*httptest.Server, string, int, error) {
@@ -25,18 +27,18 @@ func setupMockServer() (*httptest.Server, string, int, error) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		response := fmt.Sprintf(`{
-            "versions": {
-                "values": [
-                    {
-                        "id": "v3.0",
-                        "links": [
-                            {"rel": "self", "href": "%s/v3/"}
-                        ],
-                        "status": "stable"
-                    }
-                ]
-            }
-        }`, identityServerURL)
+			"versions": {
+				"values": [
+					{
+						"id": "v3.0",
+						"links": [
+							{"rel": "self", "href": "%s/v3/"}
+						],
+						"status": "stable"
+					}
+				]
+			}
+		}`, identityServerURL)
 		fmt.Fprint(w, response)
 	})
 
@@ -48,98 +50,101 @@ func setupMockServer() (*httptest.Server, string, int, error) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("X-Subject-Token", "MIIFvgY")
 		w.WriteHeader(http.StatusCreated)
-		response := `{
-            "token": {
-                "methods": ["password"],
-                "project": {
-                    "domain": {
-                        "id": "default",
-                        "name": "Default"
-                    },
-                    "id": "8538a3f13f9541b28c2620eb19065e45",
-                    "name": "admin"
-                },
+		response := fmt.Sprintf(`{
+			"token": {
+				"methods": ["password"],
+				"project": {
+					"domain": {
+						"id": "default",
+						"name": "Default"
+					},
+					"id": "8538a3f13f9541b28c2620eb19065e45",
+					"name": "admin"
+				},
 				"catalog": [
-				{
-					"type": "image",
-                    "name": "glance",
-							"endpoints": [{
-								"url": "http://localhost:%d/v2/images",
-								"region": "RegionOne",
-								"interface": "public",
-								"id": "29beb2f1567642eb810b042b6719ea88"
-
-							}]
-						}],
-                "user": {
-                    "domain": {
-                        "id": "default",
-                        "name": "Default"
-                    },
-                    "id": "3ec3164f750146be97f21559ee4d9c51",
-                    "name": "admin"
-                },
-                "issued_at": "201406-10T20:55:16.806027Z"
-            }
-        }`
-		response = fmt.Sprintf(response, port)
+					{
+						"type": "image",
+						"name": "glance",
+						"endpoints": [{
+							"url": "http://localhost:%d/v2/images",
+							"region": "RegionOne",
+							"interface": "public",
+							"id": "29beb2f1567642eb810b042b6719ea88"
+						}]
+					}
+				],
+				"user": {
+					"domain": {
+						"id": "default",
+						"name": "Default"
+					},
+					"id": "3ec3164f750146be97f21559ee4d9c51",
+					"name": "admin"
+				},
+				"issued_at": "201406-10T20:55:16.806027Z"
+			}
+		}`, port)
 		fmt.Fprint(w, response)
 	})
 
 	server := httptest.NewUnstartedServer(mux)
 	server.Listener = listener
-
 	server.Start()
 
 	return server, identityServerURL, port, nil
 }
 
-func TestPopulate(t *testing.T) {
-	os.Setenv("username", "testuser")
-	os.Setenv("password", "testpassword")
-	os.Setenv("projectID", "testproject")
-	os.Setenv("domainName", "testdomain")
+var _ = ginkgo.Describe("Populate Test", func() {
+	var (
+		server            *httptest.Server
+		identityServerURL string
+		port              int
+		err               error
+	)
 
-	server, identityServerURL, port, err := setupMockServer()
-	if err != nil {
-		t.Fatalf("Failed to start mock server: %v", err)
-	}
-	defer server.Close()
+	ginkgo.BeforeEach(func() {
+		os.Setenv("username", "testuser")
+		os.Setenv("password", "testpassword")
+		os.Setenv("projectID", "testproject")
+		os.Setenv("domainName", "testdomain")
 
-	fmt.Printf("Mock server running on port: %d\n", port)
+		server, identityServerURL, port, err = setupMockServer()
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		ginkgo.GinkgoWriter.Printf("Mock server running on port: %d\n", port)
+	})
 
-	fileName := "disk.img"
-	secretName := "test-secret"
-	imageID := "test-image-id"
-	ownerUID := "test-uid"
-	config := &appConfig{
-		identityEndpoint: identityServerURL,
-		secretName:       secretName,
-		imageID:          imageID,
-		ownerUID:         ownerUID,
-		pvcSize:          100,
-		volumePath:       fileName,
-	}
+	ginkgo.AfterEach(func() {
+		server.Close()
+	})
 
-	populate(config)
+	ginkgo.Describe("Testing the population of image data", func() {
+		ginkgo.It("should correctly populate the image data from the mock server", func() {
+			fileName := "disk.img"
+			secretName := "test-secret"
+			imageID := "test-image-id"
+			ownerUID := "test-uid"
+			config := &appConfig{
+				identityEndpoint: identityServerURL,
+				secretName:       secretName,
+				imageID:          imageID,
+				ownerUID:         ownerUID,
+				pvcSize:          100,
+				volumePath:       fileName,
+			}
 
-	file, err := os.Open(fileName)
-	if err != nil {
-		t.Fatalf("Failed to open file: %v", err)
-	}
-	defer file.Close()
+			populate(config)
 
-	content, err := io.ReadAll(file)
-	if err != nil {
-		t.Fatalf("Failed to read file: %v", err)
-	}
+			file, err := os.Open(fileName)
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			defer file.Close()
 
-	if string(content) != "mock_data\n" {
-		t.Errorf("Expected %s, got %s", "mock_data\n", string(content))
-	}
+			content, err := io.ReadAll(file)
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-	err = os.Remove(fileName)
-	if err != nil {
-		t.Errorf("Failed to delete file: %v", err)
-	}
-}
+			gomega.Expect(string(content)).To(gomega.Equal("mock_data\n"))
+
+			err = os.Remove(fileName)
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		})
+	})
+})
