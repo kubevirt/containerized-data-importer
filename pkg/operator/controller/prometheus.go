39cc977406927440b43f3e46b859bb89784b4d42
Arnon Gilboa <agilboa@redhat.com>
2024-05-05 16:44:12 +0300

    [release-v1.57] CDIStorageProfilesIncomplete should not impact operator health (#3237)
    Manual backport of #3232
    
    The alert caused OpenShift Virtualization operator status go degraded.
    
    Add unsupported provisioners that does not work with CDI and shouldn't
    cause CDIStorageProfilesIncomplete alert.
    
    Fixes CNV-39569
    
    Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

--- pkg/operator/controller/prometheus.go
+++ pkg/operator/controller/prometheus.go
@@ -201,7 +201,7 @@ func getAlertRules(runbookURLTemplate string) []promv1.Rule {
 			},
 			map[string]string{
 				severityAlertLabelKey:     "info",
-				healthImpactAlertLabelKey: "warning",
+				healthImpactAlertLabelKey: "none",
 				partOfAlertLabelKey:       partOfAlertLabelValue,
 				componentAlertLabelKey:    componentAlertLabelValue,
 			},
