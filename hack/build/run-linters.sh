cd4ceb619bd54cd7f6e659b669a6463d0d15736b
Alex Kalenyuk <akalenyu@redhat.com>
2024-05-08 18:48:45 +0300

    Bump golangci version due to panic
    Bumping to avoid a panic with 1.22.3
    ```
    ERRO [runner] Panic: SA1027: package "main" (isInitialPkg: true, needAnalyzeSource: true): runtime error: invalid memory address or nil pointer dereference: goroutine 20883 [running]:
    runtime/debug.Stack()
    	/gimme/.gimme/versions/go1.22.3.linux.amd64/src/runtime/debug/stack.go:24 +0x5e
    ```
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- hack/build/run-linters.sh
+++ hack/build/run-linters.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-GOLANGCI_VERSION="${GOLANGCI_VERSION:-v1.54.2}"
+GOLANGCI_VERSION="${GOLANGCI_VERSION:-v1.58.0}"
 
 go install "github.com/golangci/golangci-lint/cmd/golangci-lint@${GOLANGCI_VERSION}"
 
