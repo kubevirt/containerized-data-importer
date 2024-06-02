kubevirt/containerized-data-importer

commit a5cedb15559752e66566a467a840376fd3ad18c7

Progress metrics refactor and rename (#3254)
* clone_progress metric refactor

The clone_progress metric is not in the monitoring package. The metric
is with incorrect name, based on the kubevirt and Prometheus metrics
naming conventions. It's not documented and not located under
/pkg/monitoring. After the code refactoring we should not have
Prometheus metrics in other places in the code, other than the
/monitoring/metrics package, and metrics should be registered using
operator-observability package.

Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

* openstack_populator_progress metric refactor

Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

* clone_progress metric refactor CR fixes

Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

* ovirt_progress metric refactor

Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

* Align progress metrics names with linter rules

Also add the metrics to the doc and json generation tools.

Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

* Remove redundant ListMetrics

Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

---------

Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

pkg/monitoring/metrics/cdi-cloner/BUILD.bazel
@@ -1,0 +1,16 @@
+load("@io_bazel_rules_go//go:def.bzl", "go_library")
+
+go_library(
+    name = "go_default_library",
+    srcs = [
+        "clone_metrics.go",
+        "metrics.go",
+    ],
+    importpath = "kubevirt.io/containerized-data-importer/pkg/monitoring/metrics/cdi-cloner",
+    visibility = ["//visibility:public"],
+    deps = [
+        "//vendor/github.com/machadovilaca/operator-observability/pkg/operatormetrics:go_default_library",
+        "//vendor/github.com/prometheus/client_model/go:go_default_library",
+        "//vendor/sigs.k8s.io/controller-runtime/pkg/metrics:go_default_library",
+    ],
+)

pkg/monitoring/metrics/cdi-cloner/metrics.go
@@ -1,4 +1,4 @@
-package cdicontroller
+package cdicloner
 
 import (
 	"github.com/machadovilaca/operator-observability/pkg/operatormetrics"
@@ -9,9 +9,8 @@ import (
 // SetupMetrics register prometheus metrics
 func SetupMetrics() error {
 	operatormetrics.Register = runtimemetrics.Registry.Register
+	operatormetrics.Unregister = runtimemetrics.Registry.Unregister
 	return operatormetrics.RegisterMetrics(
-		dataImportCronMetrics,
-		storageMetrics,
-		dataVolumeMetrics,
+		clonerMetrics,
 	)
 }

cmd/cdi-cloner/BUILD.bazel
@@ -9,10 +9,10 @@ go_library(
     visibility = ["//visibility:private"],
     deps = [
         "//pkg/common:go_default_library",
+        "//pkg/monitoring/metrics/cdi-cloner:go_default_library",
         "//pkg/util:go_default_library",
         "//pkg/util/prometheus:go_default_library",
         "//vendor/github.com/golang/snappy:go_default_library",
-        "//vendor/github.com/prometheus/client_golang/prometheus:go_default_library",
         "//vendor/k8s.io/klog/v2:go_default_library",
     ],
 )

cmd/openstack-populator/BUILD.bazel
@@ -6,13 +6,12 @@ go_library(
     importpath = "kubevirt.io/containerized-data-importer/cmd/openstack-populator",
     visibility = ["//visibility:private"],
     deps = [
+        "//pkg/monitoring/metrics/openstack-populator:go_default_library",
         "//pkg/util/prometheus:go_default_library",
         "//vendor/github.com/gophercloud/gophercloud:go_default_library",
         "//vendor/github.com/gophercloud/gophercloud/openstack:go_default_library",
         "//vendor/github.com/gophercloud/gophercloud/openstack/imageservice/v2/imagedata:go_default_library",
         "//vendor/github.com/gophercloud/utils/openstack/clientconfig:go_default_library",
-        "//vendor/github.com/prometheus/client_golang/prometheus:go_default_library",
-        "//vendor/github.com/prometheus/client_model/go:go_default_library",
         "//vendor/k8s.io/klog/v2:go_default_library",
     ],
 )

cmd/ovirt-populator/BUILD.bazel
@@ -6,9 +6,8 @@ go_library(
     importpath = "kubevirt.io/containerized-data-importer/cmd/ovirt-populator",
     visibility = ["//visibility:private"],
     deps = [
+        "//pkg/monitoring/metrics/ovirt-populator:go_default_library",
         "//pkg/util/prometheus:go_default_library",
-        "//vendor/github.com/prometheus/client_golang/prometheus:go_default_library",
-        "//vendor/github.com/prometheus/client_model/go:go_default_library",
         "//vendor/k8s.io/klog/v2:go_default_library",
     ],
 )

pkg/image/BUILD.bazel
@@ -13,12 +13,11 @@ go_library(
     visibility = ["//visibility:public"],
     deps = [
         "//pkg/common:go_default_library",
+        "//pkg/monitoring/metrics/cdi-cloner:go_default_library",
         "//pkg/system:go_default_library",
         "//pkg/util:go_default_library",
         "//vendor/github.com/docker/go-units:go_default_library",
         "//vendor/github.com/pkg/errors:go_default_library",
-        "//vendor/github.com/prometheus/client_golang/prometheus:go_default_library",
-        "//vendor/github.com/prometheus/client_model/go:go_default_library",
         "//vendor/k8s.io/apimachinery/pkg/api/resource:go_default_library",
         "//vendor/k8s.io/klog/v2:go_default_library",
     ],
@@ -34,12 +33,11 @@ go_test(
     embed = [":go_default_library"],
     deps = [
         "//pkg/common:go_default_library",
+        "//pkg/monitoring/metrics/cdi-cloner:go_default_library",
         "//pkg/system:go_default_library",
         "//vendor/github.com/onsi/ginkgo/v2:go_default_library",
         "//vendor/github.com/onsi/gomega:go_default_library",
         "//vendor/github.com/pkg/errors:go_default_library",
-        "//vendor/github.com/prometheus/client_golang/prometheus:go_default_library",
-        "//vendor/github.com/prometheus/client_model/go:go_default_library",
         "//vendor/k8s.io/apimachinery/pkg/api/resource:go_default_library",
     ],
 )

pkg/importer/BUILD.bazel
@@ -22,6 +22,7 @@ go_library(
     deps = [
         "//pkg/common:go_default_library",
         "//pkg/image:go_default_library",
+        "//pkg/monitoring/metrics/cdi-cloner:go_default_library",
         "//pkg/util:go_default_library",
         "//pkg/util/prometheus:go_default_library",
         "//staging/src/kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1:go_default_library",
@@ -41,14 +42,12 @@ go_library(
         "//vendor/github.com/ovirt/go-ovirt-client:go_default_library",
         "//vendor/github.com/ovirt/go-ovirt-client-log-klog:go_default_library",
         "//vendor/github.com/pkg/errors:go_default_library",
-        "//vendor/github.com/prometheus/client_golang/prometheus:go_default_library",
         "//vendor/github.com/ulikunitz/xz:go_default_library",
         "//vendor/google.golang.org/api/option:go_default_library",
         "//vendor/k8s.io/apimachinery/pkg/api/resource:go_default_library",
         "//vendor/k8s.io/klog/v2:go_default_library",
     ] + select({
         "@io_bazel_rules_go//go/platform:amd64": [
-            "//vendor/github.com/prometheus/client_model/go:go_default_library",
             "//vendor/github.com/vmware/govmomi:go_default_library",
             "//vendor/github.com/vmware/govmomi/find:go_default_library",
             "//vendor/github.com/vmware/govmomi/object:go_default_library",

pkg/monitoring/metrics/openstack-populator/BUILD.bazel
@@ -1,0 +1,16 @@
+load("@io_bazel_rules_go//go:def.bzl", "go_library")
+
+go_library(
+    name = "go_default_library",
+    srcs = [
+        "metrics.go",
+        "populator_metrics.go",
+    ],
+    importpath = "kubevirt.io/containerized-data-importer/pkg/monitoring/metrics/openstack-populator",
+    visibility = ["//visibility:public"],
+    deps = [
+        "//vendor/github.com/machadovilaca/operator-observability/pkg/operatormetrics:go_default_library",
+        "//vendor/github.com/prometheus/client_model/go:go_default_library",
+        "//vendor/sigs.k8s.io/controller-runtime/pkg/metrics:go_default_library",
+    ],
+)

commit c61f615bde5ced2a495250809bb622fd919d8aa8

Fix progress metric registration and parsing
Use default metric registration. We shouldn't use the controller-runtime
registration as we have no controller here and it will not register the
metric correctly.

Fix the metric parsing to match its new name. Otherwise the DV progress
will not be updated until its 100%.

Regression introduced in #3254

Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

pkg/controller/datavolume/controller-base.go
@@ -1110,7 +1110,7 @@ func updateProgressUsingPod(dataVolumeCopy *cdiv1.DataVolume, pod *corev1.Pod) e
 	}
 
 	// Example value: import_progress{ownerUID="b856691e-1038-11e9-a5ab-525500d15501"} 13.45
-	var importRegExp = regexp.MustCompile("progress\\{ownerUID\\=\"" + string(dataVolumeCopy.UID) + "\"\\} (\\d{1,3}\\.?\\d*)")
+	var importRegExp = regexp.MustCompile("progress_total\\{ownerUID\\=\"" + string(dataVolumeCopy.UID) + "\"\\} (\\d{1,3}\\.?\\d*)")
 	if progressReport, err := cc.GetProgressReportFromURL(url, importRegExp, httpClient); err != nil {
 		return err
 	} else if progressReport != "" {

pkg/monitoring/metrics/cdi-cloner/BUILD.bazel
@@ -11,6 +11,5 @@ go_library(
     deps = [
         "//vendor/github.com/machadovilaca/operator-observability/pkg/operatormetrics:go_default_library",
         "//vendor/github.com/prometheus/client_model/go:go_default_library",
-        "//vendor/sigs.k8s.io/controller-runtime/pkg/metrics:go_default_library",
     ],
 )

pkg/monitoring/metrics/cdi-cloner/metrics.go
@@ -2,14 +2,10 @@ package cdicloner
 
 import (
 	"github.com/machadovilaca/operator-observability/pkg/operatormetrics"
-
-	runtimemetrics "sigs.k8s.io/controller-runtime/pkg/metrics"
 )
 
 // SetupMetrics register prometheus metrics
 func SetupMetrics() error {
-	operatormetrics.Register = runtimemetrics.Registry.Register
-	operatormetrics.Unregister = runtimemetrics.Registry.Unregister
 	return operatormetrics.RegisterMetrics(
 		clonerMetrics,
 	)