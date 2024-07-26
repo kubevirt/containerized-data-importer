368ad80a284c462c49eeacb1abc52f76bc3a305b
Alex Kalenyuk <akalenyu@redhat.com>
2024-07-26 07:25:15 +0300

    Update kubevirtci, 1.30 lanes (#3343)
    * Bump kvci to latest
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>
    
    * Switch testing lanes to 1.30/1.29
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>
    
    * Revert "Add W/A for NFS OOMKills"
    
    https://github.com/kubevirt/kubevirt/issues/10822#issuecomment-2220620883
    
    This reverts commit aba780300f863942346903efa61bde8e9ddbaef7.
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>
    
    ---------
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- hack/update-kubevirtci.sh
+++ hack/update-kubevirtci.sh
@@ -17,7 +17,7 @@
 SCRIPT_ROOT="$(cd "$(dirname $0)/../" && pwd -P)"
 
 # the kubevirtci tag to vendor from (https://github.com/kubevirt/kubevirtci/tags)
-kubevirtci_release_tag=2402231446-3191285
+kubevirtci_release_tag=2407190939-76a2781d
 
 # remove previous cluster-up dir entirely before vendoring
 rm -rf ${SCRIPT_ROOT}/cluster-up
