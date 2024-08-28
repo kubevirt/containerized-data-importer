1862f3f4cd696e9754818cceb6e1be701e640f52
Thomas-David Griedel <griedel911@gmail.com>
2024-08-28 16:57:18 +0200

    Remove unused ADHOC_BUILDER env check
    Signed-off-by: Thomas-David Griedel <griedel911@gmail.com>

--- hack/build/bazel-build-builder.sh
+++ hack/build/bazel-build-builder.sh
@@ -33,32 +33,27 @@ if [ "${CDI_CONTAINER_BUILDCMD}" = "buildah" ]; then
     echo "Building with $BUILDAH_PLATFORM_FLAG"
 fi
 
-# The other circumstance in which we need to build the builder image is
-# in the course of test and development of the builder image itself.
-# we'll signal this circumstance by setting the env variable ADHOC_BUILDER
-#
-# also instead of hard-coding the UNTAGGED_BUILDER_IMAGE, we're going
+
+# Instead of hard-coding the UNTAGGED_BUILDER_IMAGE, we're going
 # to use DOCKER_PREFIX as it is set in config.sh and used elsewhere in
 # the build system, to introduce a little more consistency
 
-if [ x"${ADHOC_BUILDER}" != "x" ]; then
-    BUILDER_SPEC="${BUILD_DIR}/docker/builder"
-    UNTAGGED_BUILDER_IMAGE="${DOCKER_PREFIX}/kubevirt-cdi-bazel-builder"
-    BUILDER_TAG=$(date +"%y%m%d%H%M")-$(git rev-parse --short HEAD)
-    BUILDER_MANIFEST=${UNTAGGED_BUILDER_IMAGE}:${BUILDER_TAG}
-    echo "export BUILDER_IMAGE=$BUILDER_MANIFEST"
+BUILDER_SPEC="${BUILD_DIR}/docker/builder"
+UNTAGGED_BUILDER_IMAGE="${DOCKER_PREFIX}/kubevirt-cdi-bazel-builder"
+BUILDER_TAG=$(date +"%y%m%d%H%M")-$(git rev-parse --short HEAD)
+BUILDER_MANIFEST=${UNTAGGED_BUILDER_IMAGE}:${BUILDER_TAG}
+echo "export BUILDER_IMAGE=$BUILDER_MANIFEST"
 
-    #Build the encapsulated compile and test container
-    if [ "${CDI_CONTAINER_BUILDCMD}" = "buildah" ]; then
-        (cd ${BUILDER_SPEC} && buildah build ${BUILDAH_PLATFORM_FLAG} --build-arg ARCH=${ARCH} --manifest ${BUILDER_MANIFEST} .)
-        buildah manifest push --all ${BUILDER_MANIFEST} docker://${BUILDER_MANIFEST}
-        DIGEST=$(podman inspect $(podman images | grep ${UNTAGGED_BUILDER_IMAGE} | grep ${BUILDER_TAG} | awk '{ print $3 }') | jq '.[]["Digest"]')
-    else
-        (cd ${BUILDER_SPEC} && docker build --build-arg ARCH=${ARCH} --tag ${BUILDER_MANIFEST} .)
-        docker push ${BUILDER_MANIFEST}
-        DIGEST=$(docker images --digests | grep ${UNTAGGED_BUILDER_IMAGE} | grep ${BUILDER_TAG} | awk '{ print $4 }')
-    fi
-
-    echo "Image: ${BUILDER_MANIFEST}"
-    echo "Digest: ${DIGEST}"
+#Build the encapsulated compile and test container
+if [ "${CDI_CONTAINER_BUILDCMD}" = "buildah" ]; then
+    (cd ${BUILDER_SPEC} && buildah build ${BUILDAH_PLATFORM_FLAG} --build-arg ARCH=${ARCH} --manifest ${BUILDER_MANIFEST} .)
+    buildah manifest push --all ${BUILDER_MANIFEST} docker://${BUILDER_MANIFEST}
+    DIGEST=$(podman inspect $(podman images | grep ${UNTAGGED_BUILDER_IMAGE} | grep ${BUILDER_TAG} | awk '{ print $3 }') | jq '.[]["Digest"]')
+else
+    (cd ${BUILDER_SPEC} && docker build --build-arg ARCH=${ARCH} --tag ${BUILDER_MANIFEST} .)
+    docker push ${BUILDER_MANIFEST}
+    DIGEST=$(docker images --digests | grep ${UNTAGGED_BUILDER_IMAGE} | grep ${BUILDER_TAG} | awk '{ print $4 }')
 fi
+
+echo "Image: ${BUILDER_MANIFEST}"
+echo "Digest: ${DIGEST}"
