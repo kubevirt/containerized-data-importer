bc7cf466203fe36fde73bbcaf5c271e62c48499c
cfillekes <cfilleke@redhat.com>
2024-08-29 00:07:26 -0400

    cross-compile docker builder for s390x (#3382)
    * issue #3289 add s390x cross-build libraries to CDI builder container
    
    Signed-off-by: cfilleke <cfilleke@redhat.com>
    
    * issue #3289 add s390x cross-build libraries to CDI builder correct ARCH set error message
    
    Signed-off-by: cfilleke <cfilleke@redhat.com>
    
    * Update hack/build/docker/builder/Dockerfile
    
    Co-authored-by: Jan Schintag <jan.schintag@de.ibm.com>
    Signed-off-by: cfillekes <cfilleke@redhat.com>
    
    ---------
    
    Signed-off-by: cfilleke <cfilleke@redhat.com>
    Signed-off-by: cfillekes <cfilleke@redhat.com>
    Co-authored-by: Jan Schintag <jan.schintag@de.ibm.com>

--- hack/build/docker/builder/output-bazel-arch.sh
+++ hack/build/docker/builder/output-bazel-arch.sh
@@ -6,8 +6,11 @@ x86_64* | i?86_64* | amd64*)
 aarch64* | arm64*)
     ARCH="arm64"
     ;;
+s390x)
+    ARCH="s390x"
+    ;;
 *)
-    echo "invalid Arch, only support x86_64, aarch64"
+    echo "invalid Arch, only support x86_64, aarch64, s390x"
     exit 1
     ;;
 esac
