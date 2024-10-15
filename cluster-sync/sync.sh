8c4c21a747796143520508ad2a62a0749f7c2df5
Thomas-David Griedel <griedel911@gmail.com>
2024-10-15 15:07:05 +0200

    exclude unsupported test images
    Signed-off-by: Thomas-David Griedel <griedel911@gmail.com>

--- cluster-sync/sync.sh
+++ cluster-sync/sync.sh
@@ -180,10 +180,15 @@ if [ "${CDI_SYNC}" == "test-infra" ]; then
   _kubectl apply -f "./_out/manifests/test-proxy.yaml"
   _kubectl apply -f "./_out/manifests/sample-populator.yaml"
   _kubectl apply -f "./_out/manifests/uploadproxy-nodeport.yaml"
-  # Imageio test service:
-  _kubectl apply -f "./_out/manifests/imageio.yaml"
-  # vCenter (VDDK) test service:
-  _kubectl apply -f "./_out/manifests/vcenter.yaml"
+
+  # Check if CPU architecture is s390x
+  if [ "$(uname -m)" != "s390x" ]; then
+    # Imageio test service:
+    _kubectl apply -f "./_out/manifests/imageio.yaml"
+    # vCenter (VDDK) test service:
+    _kubectl apply -f "./_out/manifests/vcenter.yaml"
+  fi
+ 
   if _kubectl get crd securitycontextconstraints.security.openshift.io >/dev/null 2>&1; then
     _kubectl apply -f "./_out/manifests/cdi-testing-scc.yaml"
   fi
