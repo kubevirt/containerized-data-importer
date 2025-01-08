ca971b6e4e15f0cb9b5c143dbd6cc9f8456f2ea4
Matthew Arnold <marnold@redhat.com>
2025-01-08 10:03:10 -0500

    Move VDDK configuration test back, change test ID.
    Signed-off-by: Matthew Arnold <marnold@redhat.com>

--- tests/framework/vddk.go
+++ tests/framework/vddk.go
@@ -12,38 +12,6 @@ import (
 	"kubevirt.io/containerized-data-importer/tests/utils"
 )
 
-// CreateVddkDataVolume returns a VDDK data volume
-func (f *Framework) CreateVddkDataVolume(dataVolumeName, size, url string) *cdiv1.DataVolume {
-	// Find vcenter-simulator pod
-	pod, err := utils.FindPodByPrefix(f.K8sClient, f.CdiInstallNs, "vcenter-deployment", "app=vcenter")
-	gomega.Expect(err).ToNot(gomega.HaveOccurred())
-	gomega.Expect(pod).ToNot(gomega.BeNil())
-
-	// Get test VM UUID
-	id, err := f.RunKubectlCommand("exec", "-n", pod.Namespace, pod.Name, "--", "cat", "/tmp/vmid")
-	gomega.Expect(err).ToNot(gomega.HaveOccurred())
-	vmid, err := uuid.Parse(strings.TrimSpace(id))
-	gomega.Expect(err).ToNot(gomega.HaveOccurred())
-
-	// Get disk name
-	disk, err := f.RunKubectlCommand("exec", "-n", pod.Namespace, pod.Name, "--", "cat", "/tmp/vmdisk")
-	gomega.Expect(err).ToNot(gomega.HaveOccurred())
-	disk = strings.TrimSpace(disk)
-	gomega.Expect(err).ToNot(gomega.HaveOccurred())
-
-	// Create VDDK login secret
-	stringData := map[string]string{
-		common.KeyAccess: "user",
-		common.KeySecret: "pass",
-	}
-	backingFile := disk
-	secretRef := "vddksecret"
-	thumbprint := "testprint"
-	s, _ := utils.CreateSecretFromDefinition(f.K8sClient, utils.NewSecretDefinition(nil, stringData, nil, f.Namespace.Name, secretRef))
-
-	return utils.NewDataVolumeWithVddkImport(dataVolumeName, size, backingFile, s.Name, thumbprint, url, vmid.String())
-}
-
 // CreateVddkWarmImportDataVolume fetches snapshot information from vcsim and returns a multi-stage VDDK data volume
 func (f *Framework) CreateVddkWarmImportDataVolume(dataVolumeName, size, url string) *cdiv1.DataVolume {
 	// Find vcenter-simulator pod
