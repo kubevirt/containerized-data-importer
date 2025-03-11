85888767f69dcd585caafd95683e31df8f682a3d
Arnon Gilboa <agilboa@redhat.com>
2025-03-11 23:10:04 +0200

    Add GCP pd.csi.storage.gke.io capabilities (#3663)
    - Add RWX Block to supported hyperdisk types
    - Use snapshot for clone and for DataImportCron created boot sources
    
    Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

--- pkg/storagecapabilities/storagecapabilities.go
+++ pkg/storagecapabilities/storagecapabilities.go
@@ -80,8 +80,9 @@ var CapabilitiesByProvisionerKey = map[string][]StorageCapabilities{
 	"kubernetes.io/azure-file": {{rwx, file}},
 	"file.csi.azure.com":       {{rwx, file}},
 	// GCE Persistent Disk
-	"kubernetes.io/gce-pd":  {{rwo, block}},
-	"pd.csi.storage.gke.io": {{rwo, block}},
+	"kubernetes.io/gce-pd":            {{rwo, block}},
+	"pd.csi.storage.gke.io":           {{rwo, block}},
+	"pd.csi.storage.gke.io/hyperdisk": {{rwx, block}, {rwo, block}, {rwo, file}},
 	// Hitachi
 	"hspc.csi.hitachi.com": {{rwx, block}, {rwo, block}, {rwo, file}},
 	// HPE
@@ -136,6 +137,8 @@ var SourceFormatsByProvisionerKey = map[string]cdiv1.DataImportCronSourceFormat{
 	"openshift-storage.rbd.csi.ceph.com": cdiv1.DataImportCronSourceFormatSnapshot,
 	"csi.trident.netapp.io/ontap-nas":    cdiv1.DataImportCronSourceFormatSnapshot,
 	"csi.trident.netapp.io/ontap-san":    cdiv1.DataImportCronSourceFormatSnapshot,
+	"pd.csi.storage.gke.io":              cdiv1.DataImportCronSourceFormatSnapshot,
+	"pd.csi.storage.gke.io/hyperdisk":    cdiv1.DataImportCronSourceFormatSnapshot,
 }
 
 // CloneStrategyByProvisionerKey defines the advised clone strategy for a provisioner
@@ -163,6 +166,8 @@ var CloneStrategyByProvisionerKey = map[string]cdiv1.CDICloneStrategy{
 	"csi.trident.netapp.io/ontap-nas":          cdiv1.CloneStrategySnapshot,
 	"csi.trident.netapp.io/ontap-san":          cdiv1.CloneStrategySnapshot,
 	"kubesan.gitlab.io":                        cdiv1.CloneStrategyCsiClone,
+	"pd.csi.storage.gke.io":                    cdiv1.CloneStrategySnapshot,
+	"pd.csi.storage.gke.io/hyperdisk":          cdiv1.CloneStrategySnapshot,
 }
 
 const (
@@ -367,6 +372,14 @@ var storageClassToProvisionerKeyMapper = map[string]func(sc *storagev1.StorageCl
 			return "ebs.csi.aws.com/io2"
 		}
 		return "ebs.csi.aws.com"
+	},
+	"pd.csi.storage.gke.io": func(sc *storagev1.StorageClass) string {
+		switch sc.Parameters["type"] {
+		case "hyperdisk-balanced", "hyperdisk-balanced-high-availability":
+			return "pd.csi.storage.gke.io/hyperdisk"
+		default:
+			return "pd.csi.storage.gke.io"
+		}
 	},
 }
 
