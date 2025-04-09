4d99ee75c29b5f0e8a98a28d9341a1a5ce676fb1
Alex Kalenyuk <akalenyu@redhat.com>
2025-04-09 12:36:42 +0300

    Bump kvci for "external" NFS setup
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- hack/update-kubevirtci.sh
+++ hack/update-kubevirtci.sh
@@ -17,7 +17,7 @@
 SCRIPT_ROOT="$(cd "$(dirname $0)/../" && pwd -P)"
 
 # the kubevirtci tag to vendor from (https://github.com/kubevirt/kubevirtci/tags)
-kubevirtci_release_tag=2502072021-f3ed3dc0
+kubevirtci_release_tag=2505080952-2f84ba78
 
 # remove previous cluster-up dir entirely before vendoring
 rm -rf ${SCRIPT_ROOT}/cluster-up
