66b273880a08b77757052f5465ef907f7e7072e8
Alex Kalenyuk <akalenyu@redhat.com>
2025-04-22 15:56:01 +0300

    Revert "Add RWX block to Portworx capabilities (#3644)"
    This reverts commit 5ff0e33232c7e583d322258ea835e0494810fc79.

--- pkg/storagecapabilities/storagecapabilities.go
+++ pkg/storagecapabilities/storagecapabilities.go
@@ -97,8 +97,8 @@ var CapabilitiesByProvisionerKey = map[string][]StorageCapabilities{
 	// Portworx CSI
 	"pxd.openstorage.org/shared": createOpenStorageSharedVolumeCapabilities(),
 	"pxd.openstorage.org":        createOpenStorageSharedVolumeCapabilities(),
-	"pxd.portworx.com/shared":    {{rwx, block}, {rwx, file}, {rwo, block}, {rwo, file}},
-	"pxd.portworx.com":           {{rwx, block}, {rwx, file}, {rwo, block}, {rwo, file}},
+	"pxd.portworx.com/shared":    createOpenStorageSharedVolumeCapabilities(),
+	"pxd.portworx.com":           createOpenStorageSharedVolumeCapabilities(),
 	// Trident
 	"csi.trident.netapp.io/ontap-nas": {{rwx, file}, {rwo, file}},
 	"csi.trident.netapp.io/ontap-san": {{rwx, block}},
