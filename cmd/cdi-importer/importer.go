d4fa21594c70d49f261b5bf7936d9507aa472f21
Alex Kalenyuk <akalenyu@redhat.com>
2025-06-24 22:58:58 +0300

    log cache mode in importer
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- cmd/cdi-importer/importer.go
+++ cmd/cdi-importer/importer.go
@@ -234,6 +234,7 @@ func writeTerminationMessage(termMsg *common.TerminationMessage) error {
 
 func newDataProcessor(contentType string, volumeMode v1.PersistentVolumeMode, ds importer.DataSourceInterface, imageSize string, filesystemOverhead float64, preallocation bool) *importer.DataProcessor {
 	dest := getImporterDestPath(contentType, volumeMode)
+	klog.Infof("CACHE MODE: %s\n", os.Getenv(common.CacheMode))
 	processor := importer.NewDataProcessor(ds, dest, common.ImporterDataDir, common.ScratchDataDir, imageSize, filesystemOverhead, preallocation, os.Getenv(common.CacheMode))
 	return processor
 }
