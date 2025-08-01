483ecf905abf19f8f7281a7fb01617b0c471c791
Alex Kalenyuk <akalenyu@redhat.com>
2025-08-01 10:01:16 +0300

    Bump kvci
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- hack/update-kubevirtci.sh
+++ hack/update-kubevirtci.sh
@@ -17,7 +17,7 @@
 SCRIPT_ROOT="$(cd "$(dirname $0)/../" && pwd -P)"
 
 # the kubevirtci tag to vendor from (https://github.com/kubevirt/kubevirtci/tags)
-kubevirtci_release_tag=2506041554-5bb73299
+kubevirtci_release_tag=2507291802-0f560a31
 
 # remove previous cluster-up dir entirely before vendoring
 rm -rf ${SCRIPT_ROOT}/cluster-up
