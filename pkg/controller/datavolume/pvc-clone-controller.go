901cab0700278850c936867d336f3a568b7cd1f3
Arnon Gilboa <agilboa@redhat.com>
2025-08-07 17:13:33 +0300

    wip
    Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

--- pkg/controller/datavolume/pvc-clone-controller.go
+++ pkg/controller/datavolume/pvc-clone-controller.go
@@ -445,7 +445,7 @@ func (r *PvcCloneReconciler) validateCloneAndSourcePVC(syncState *dvSyncState, l
 		return false, err
 	}
 
-	err = r.validateClone(sourcePvc, &datavolume.Spec)
+	err = r.validateClone(sourcePvc, syncState)
 	if err != nil {
 		syncErr := r.syncDataVolumeStatusPhaseWithEvent(syncState, datavolume.Status.Phase, nil,
 			Event{
@@ -463,36 +463,22 @@ func (r *PvcCloneReconciler) validateCloneAndSourcePVC(syncState *dvSyncState, l
 }
 
 // validateClone compares a clone spec against its source PVC to validate its creation
-func (r *PvcCloneReconciler) validateClone(sourcePVC *corev1.PersistentVolumeClaim, spec *cdiv1.DataVolumeSpec) error {
-	var targetResources corev1.VolumeResourceRequirements
-	var err error
-
+func (r *PvcCloneReconciler) validateClone(sourcePVC *corev1.PersistentVolumeClaim, syncState *dvSyncState) error {
+	spec := &syncState.dvMutated.Spec
 	valid, sourceContentType, targetContentType := validateContentTypes(sourcePVC, spec)
 	if !valid {
 		msg := fmt.Sprintf("Source contentType (%s) and target contentType (%s) do not match", sourceContentType, targetContentType)
 		return errors.New(msg)
 	}
 
-	isSizelessClone := false
+	isTargetStorageWithSize := false
 	explicitPvcRequest := spec.PVC != nil
-	if explicitPvcRequest {
-		targetResources = spec.PVC.Resources
-	} else {
-		// The storage size in the target DV can be empty
-		// when cloning using the 'Storage' API
-		if _, ok := spec.Storage.Resources.Requests[corev1.ResourceStorage]; !ok {
-			isSizelessClone = true
-		}
-		targetResources, err = cc.GetEffectiveStorageResources(context.TODO(), r.client, spec.Storage.Resources, spec.Storage.StorageClassName, spec.ContentType, r.log)
-		if err != nil {
-			return err
-		}
+	if !explicitPvcRequest {
+		_, isTargetStorageWithSize = spec.Storage.Resources.Requests[corev1.ResourceStorage]
 	}
 
-	// TODO: Spec.Storage API needs a better more complex check to validate clone size - to account for fsOverhead
-	// simple size comparison will not work here
-	if (!isSizelessClone && cc.GetVolumeMode(sourcePVC) == corev1.PersistentVolumeBlock) || explicitPvcRequest {
-		if err := cc.ValidateRequestedCloneSize(sourcePVC.Spec.Resources, targetResources); err != nil {
+	if isTargetStorageWithSize || explicitPvcRequest {
+		if err := cc.ValidateRequestedCloneSize(sourcePVC.Spec.Resources, syncState.pvcSpec.Resources); err != nil {
 			return err
 		}
 	}
