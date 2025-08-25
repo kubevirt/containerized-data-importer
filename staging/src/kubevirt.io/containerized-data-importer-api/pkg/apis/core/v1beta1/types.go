e24f831089a30f61ab7fbd6feb5613db709008d4
Jack Yu <jack.yu@suse.com>
2025-08-25 20:38:59 +0800

    fix: Items type in VolumeUploadSourceList should be VolumeUploadSource
    Signed-off-by: Jack Yu <jack.yu@suse.com>

--- staging/src/kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1/types.go
+++ staging/src/kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1/types.go
@@ -164,7 +164,6 @@ type DataSourceRefSourceDataSource struct {
 	Namespace string `json:"namespace"`
 	// The name of the source DataSource
 	Name string `json:"name"`
-
 }
 
 // DataVolumeBlankImage provides the parameters to create a new raw blank image for the PVC
@@ -517,7 +516,7 @@ type DataSourceSource struct {
 	// +optional
 	Snapshot *DataVolumeSourceSnapshot `json:"snapshot,omitempty"`
 	// +optional
-	DataSource *DataSourceRefSourceDataSource `json:"dataSource,omitempty"` 
+	DataSource *DataSourceRefSourceDataSource `json:"dataSource,omitempty"`
 }
 
 // DataSourceStatus provides the most recently observed status of the DataSource
@@ -755,7 +754,7 @@ type VolumeUploadSourceList struct {
 	metav1.ListMeta `json:"metadata"`
 
 	// Items provides a list of DataSources
-	Items []VolumeImportSource `json:"items"`
+	Items []VolumeUploadSource `json:"items"`
 }
 
 const (
