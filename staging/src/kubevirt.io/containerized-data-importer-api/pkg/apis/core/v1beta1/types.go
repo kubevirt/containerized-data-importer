2b04a3d17a89c38764b8014c77da12c53b446f78
Noam Assouline <nassouli@redhat.com>
2025-09-25 12:05:53 +0300

    Add status subresource to StorageProfile CRD (#3863)
    it includes:
    1. Add +kubebuilder:subresource:status annotation to StorageProfile type
    2. Update controller to handle status updates separately from spec/metadata
    3. Regenerate CRD with status subresource support
    
    Signed-off-by: Noam Assouline <nassouli@redhat.com>

--- staging/src/kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1/types.go
+++ staging/src/kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1/types.go
@@ -428,6 +428,7 @@ const DataVolumeCloneSourceSubresource = "source"
 // +kubebuilder:object:root=true
 // +kubebuilder:storageversion
 // +kubebuilder:resource:scope=Cluster
+// +kubebuilder:subresource:status
 type StorageProfile struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
