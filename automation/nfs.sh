c3756c807e207c80a82cea7dff89222a2f5e9821
Alex Kalenyuk <akalenyu@redhat.com>
2026-02-25 18:06:19 +0200

    Switch testing lanes to 1.35/1.34
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- automation/nfs.sh
+++ automation/nfs.sh
@@ -18,7 +18,7 @@
 #
 
 set -ex
-export TARGET=k8s-1.33
+export TARGET=k8s-1.35
 export KUBEVIRT_DEPLOY_NFS_CSI=true
 export KUBEVIRT_STORAGE=nfs
 export KUBEVIRT_NFS_DIR=${KUBEVIRT_NFS_DIR:-/var/lib/containers/nfs-data}
