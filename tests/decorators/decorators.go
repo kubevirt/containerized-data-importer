5246fc1c901b91380c86e625b8515a3791c906cc
dsanatar <dsanatar@redhat.com>
2026-09-02 14:02:03 -0400

    tests: add new label decorators
    Signed-off-by: dsanatar <dsanatar@redhat.com>

---
+++ tests/decorators/decorators.go
@@ -0,0 +1,22 @@
+package decorators
+
+import . "github.com/onsi/ginkgo/v2"
+
+var (
+
+	/* Features */
+	Istio   = Label("Istio")
+	ImageIO = Label("ImageIO")
+	VDDK    = Label("VDDK")
+
+	/* Storage classes */
+
+	// RequiresBlockStorage requires a storage class with Block storage support
+	RequiresBlockStorage = Label("RequiresBlockStorage")
+
+	// RequiresSnapshotStorageClass requires a storage class with support for snapshots
+	RequiresSnapshotStorageClass = Label("RequiresSnapshotStorageClass")
+
+	// RequiresCSICloneClass requires a storage class with support for csi clone
+	RequiresCSICloneClass = Label("RequiresCSICloneClass")
+)
