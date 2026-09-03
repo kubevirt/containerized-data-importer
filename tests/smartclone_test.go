993048725535b6e563572ba1d940c61ceccac11e
dsanatar <dsanatar@redhat.com>
2026-09-03 15:24:43 -0400

    tests: refactor clone suites to remove skips
    conditionally run test blocks based on the available
    storage class's clone strategy. in certain cases,
    forcefully use the appropriate clone strategy so
    skips can be removed.
    
    Signed-off-by: dsanatar <dsanatar@redhat.com>

--- tests/smartclone_test.go
+++ tests/smartclone_test.go
@@ -174,17 +174,30 @@ var _ = Describe("[vendor:cnv-qe@redhat.com][level:component]SmartClone tests", 
 		// Verify PVC's content
 		verifyPVC(dataVolume, f, utils.DefaultImagePath, utils.UploadFileMD5)
 	})
+})
+
+var _ = Describe("with Host-assisted clone", decorators.RequiresSnapshotStorageClass, Serial, func() {
+	f := framework.NewFramework("dv-func-test")
+
+	var orgProfileSpec *cdiv1.StorageProfileSpec
+
+	BeforeEach(func() {
+		By("Forcing host-assisted clone strategy")
+		scName := utils.DefaultStorageClass.GetName()
+		var err error
+		orgProfileSpec, err = utils.GetStorageProfileSpec(f.CdiClient, scName)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(utils.ConfigureCloneStrategy(f.CrClient, f.CdiClient, scName, orgProfileSpec, cdiv1.CloneStrategyHostAssisted)).To(Succeed())
+	})
+
+	AfterEach(func() {
+		By("Restoring original clone strategy")
+		if orgProfileSpec != nil {
+			Expect(utils.UpdateStorageProfile(f.CrClient, utils.DefaultStorageClass.GetName(), *orgProfileSpec)).To(Succeed())
+		}
+	})
 
 	It("[rfe_id:1106][test_id:3496][crit:high][vendor:cnv-qe@redhat.com][level:component] Verify DataVolume Smart Cloning - Check regular clone works", func() {
-		smartApplicable := f.IsSnapshotStorageClassAvailable()
-		sc, err := f.K8sClient.StorageV1().StorageClasses().Get(context.TODO(), f.SnapshotSCName, metav1.GetOptions{})
-		if err == nil {
-			value, ok := sc.Annotations["storageclass.kubernetes.io/is-default-class"]
-			if smartApplicable && ok && strings.Compare(value, "true") == 0 {
-				Skip("Cannot test regular cloning if Smart Clone is applicable in default Storage Class")
-			}
-		}
-
 		dataVolume, expectedMd5 := createDataVolume("dv-smart-clone-test-negative", utils.DefaultImagePath, v1.PersistentVolumeFilesystem, "", f)
 
 		// Wait for operation Succeeded
